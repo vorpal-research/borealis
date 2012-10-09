1f015b79f7a5035635331f6c920a97b47de9f656
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2012-10-09 18:10:17 +0400

    Added utility functions to support llvm->z3 conversions

--- util.h
+++ util.h
@@ -9,6 +9,9 @@
 #define UTIL_H_
 
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Value.h>
+
+#include <z3/z3++.h>
 
 namespace llvm {
 // copy the standard ostream behavior with functions
@@ -16,8 +19,43 @@ llvm::raw_ostream& operator<<(
 		llvm::raw_ostream& ost,
 		llvm::raw_ostream& (*op)(llvm::raw_ostream&));
 
+enum ConditionType {
+	EQ,
+	NEQ,
+	GT,
+	GTE,
+	LT,
+	LTE,
+	TRUE,
+	FALSE,
+	WTF
+};
+
 std::string conditionToString(const int cond);
-}
+ConditionType conditionToType(const int cond);
+
+enum ValueType {
+	INT_CONST,
+	INT_VAR,
+	BOOL_CONST,
+	BOOL_VAR,
+	REAL_CONST,
+	REAL_VAR,
+	NULL_PTR_CONST,
+	PTR_CONST,
+	PTR_VAR,
+	UNKNOWN
+};
+
+ValueType valueType(const llvm::Value& value);
+} // namespace llvm
+
+namespace z3 {
+z3::expr valueToExpr(
+		z3::context& ctx,
+		const llvm::Value& value,
+		const std::string& valueName);
+} // namespace z3
 
 namespace borealis {
 namespace util {
