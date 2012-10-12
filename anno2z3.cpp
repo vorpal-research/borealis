57475e8d06f56bd1ed8f37240a72605b62f3e53a
Mikhail Belyaev <devnull@localhost>
2012-10-12 14:57:24 +0400

    fixed anno2z3

--- anno2z3.cpp
+++ anno2z3.cpp
@@ -8,6 +8,8 @@
 #include <z3/z3++.h>
 #include <llvm/Value.h>
 
+#include <unordered_map>
+
 #include "Anno/anno.hpp"
 #include "util.h"
 
@@ -23,28 +25,35 @@ public:
     typedef z3::expr expr;
 
 private:
-    const z3_context& z3;
+    z3_context& z3;
     const name_context& names;
-    expr retVal;
+    std::unique_ptr<expr> retVal;
+
+    void assign(const expr& exp) { retVal.reset(new expr(exp)); }
 
 public:
-    z3Visitor(const z3_context& z3, const name_context& names):z3(z3), names(names) {}
+    z3Visitor(z3_context& z3, const name_context& names):z3(z3), names(names), retVal(nullptr) {}
     virtual ~z3Visitor(){}
 
-    const expr& get() { return retVal; }
+    const expr& get() { return *retVal; }
 
     virtual void onDoubleConstant(double v) {
         using borealis::util::toString;
-        retVal = z3.real_val(toString(v).c_str());
+        assign( z3.real_val(toString(v).c_str()) );
     }
     virtual void onIntConstant(int v) {
-        retVal = z3.int_val(v);
+        assign( z3.int_val(v) );
     }
     virtual void onBoolConstant(bool v) {
-        retVal = z3.bool_val(v);
+        assign( z3.bool_val(v) );
     }
 
-    virtual void onVariable(const std::string&) {
+    virtual void onVariable(const std::string& str) {
+        using z3::valueToExpr;
+        if(!!names.count(str)) {
+            auto v = names.at(str);
+            assign( valueToExpr(z3, *v, str) );
+        }
         // FIXME
     }
     virtual void onBuiltin(const std::string&) {
@@ -54,6 +63,40 @@ public:
         // do nothing, masks have nothing to do with smt
     }
 
+    expr make_mod_expr(expr const& lhv, expr const& rhv) {
+        if(lhv.is_arith() && rhv.is_arith()) {
+            return expr(z3, Z3_mk_mod(z3, lhv, rhv));
+        } else if(lhv.is_bv() && rhv.is_bv()){
+            return expr(z3, Z3_mk_bvsrem(z3, lhv, rhv));
+        } else {
+            // FIXME: do smth?
+            return expr(z3);
+        }
+    }
+
+    expr make_lsh_expr(expr const& lhv, expr const& rhv) {
+        if(lhv.is_bv() && rhv.is_bv()) {
+            return expr(z3, Z3_mk_bvshl(z3, lhv, rhv));
+        } else if(lhv.is_arith() && rhv.is_arith()) {
+            return lhv * pw(2, rhv);
+        } else {
+            // FIXME: do smth?
+            return expr(z3);
+        }
+    }
+
+    expr make_rsh_expr(expr const& lhv, expr const& rhv) {
+        if(lhv.is_arith() && rhv.is_arith()) {
+            return expr(z3, Z3_mk_bvashr(z3, lhv, rhv));
+        } else if(lhv.is_bv() && rhv.is_bv()){
+            return lhv / pw(2, rhv);
+        } else
+        {
+            // FIXME: do smth?
+            return expr(z3);
+        }
+    }
+
     virtual void onBinary(bin_opcode op, const prod_t& lhv, const prod_t& rhv) {
         z3Visitor left(z3, names);
         z3Visitor right(z3, names);
@@ -61,24 +104,27 @@ public:
         rhv->accept(right);
 
         switch(op) {
-        case OPCODE_PLUS:  retVal = left.retVal +  right.retVal; break;
-        case OPCODE_MINUS: retVal = left.retVal -  right.retVal; break;
-        case OPCODE_MULT:  retVal = left.retVal *  right.retVal; break;
-        case OPCODE_DIV:   retVal = left.retVal /  right.retVal; break;
-        case OPCODE_MOD:   retVal = left.retVal %  right.retVal; break;
-        case OPCODE_EQ:    retVal = left.retVal == right.retVal; break;
-        case OPCODE_NE:    retVal = left.retVal != right.retVal; break;
-        case OPCODE_GT:    retVal = left.retVal >  right.retVal; break;
-        case OPCODE_LT:    retVal = left.retVal <  right.retVal; break;
-        case OPCODE_GE:    retVal = left.retVal >= right.retVal; break;
-        case OPCODE_LE:    retVal = left.retVal <= right.retVal; break;
-        case OPCODE_LAND:  retVal = left.retVal && right.retVal; break;
-        case OPCODE_LOR:   retVal = left.retVal || right.retVal; break;
-        case OPCODE_BAND:  retVal = left.retVal &  right.retVal; break;
-        case OPCODE_BOR:   retVal = left.retVal |  right.retVal; break;
-        case OPCODE_XOR:   retVal = left.retVal ^  right.retVal; break;
-        case OPCODE_LSH:   retVal = left.retVal << right.retVal; break;
-        case OPCODE_RSH:   retVal = left.retVal >> right.retVal; break;
+        // cases supported by Z3 api
+        case OPCODE_PLUS:  assign( left.get() +  right.get() ); break;
+        case OPCODE_MINUS: assign( left.get() -  right.get() ); break;
+        case OPCODE_MULT:  assign( left.get() *  right.get() ); break;
+        case OPCODE_DIV:   assign( left.get() /  right.get() ); break;
+        case OPCODE_EQ:    assign( left.get() == right.get() ); break;
+        case OPCODE_NE:    assign( left.get() != right.get() ); break;
+        case OPCODE_GT:    assign( left.get() >  right.get() ); break;
+        case OPCODE_LT:    assign( left.get() <  right.get() ); break;
+        case OPCODE_GE:    assign( left.get() >= right.get() ); break;
+        case OPCODE_LE:    assign( left.get() <= right.get() ); break;
+        case OPCODE_LAND:  assign( left.get() && right.get() ); break;
+        case OPCODE_LOR:   assign( left.get() || right.get() ); break;
+        case OPCODE_BAND:  assign( left.get() &  right.get() );break;
+        case OPCODE_BOR:   assign( left.get() |  right.get() );break;
+        case OPCODE_XOR:   assign( left.get() ^  right.get() );break;
+        // cases not supported:
+
+        case OPCODE_MOD:   assign( make_mod_expr(left.get(), right.get()) ); break;
+        case OPCODE_LSH:   assign( make_lsh_expr(left.get(), right.get()) ); break;
+        case OPCODE_RSH:   assign( make_rsh_expr(left.get(), right.get()) ); break;
         }
 
     }
@@ -86,9 +132,9 @@ public:
         z3Visitor operand(z3, names);
 
         switch(op) {
-        case OPCODE_NEG:  retVal = -operand.retVal; break;
-        case OPCODE_NOT:  retVal = !operand.retVal; break;
-        case OPCODE_BNOT: retVal = ~operand.retVal; break;
+        case OPCODE_NEG:  assign( -operand.get() ); break;
+        case OPCODE_NOT:  assign( !operand.get() ); break;
+        case OPCODE_BNOT: assign( ~operand.get() ); break;
         }
     }
 
