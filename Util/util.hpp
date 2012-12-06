eab01666a7cddf179a3486fc23341b04d768e6b1
Mikhail Belyaev <devnull@localhost>
2012-12-06 13:29:19 +0400

    Abstracted out exprRef to util

--- Util/util.hpp
+++ Util/util.hpp
@@ -34,6 +34,52 @@ RetTy sayonara(std::string file, int line, std::string where, std::string reason
     std::exit(EXIT_FAILURE);
 }
 
+
+// java-style reference
+template<class T>
+class ref {
+    T* ptr;
+
+public:
+    ref(const ref& that) = default;
+    explicit ref(const T& v): ptr(&v){};
+
+    const ref& operator=(const ref& that) = default;
+    const ref& operator=(const T& v) {
+        ptr = &v; return *this;
+    }
+
+    const T& get() const { return *ptr; }
+    T& get() { return *ptr; }
+    // XXX: remove?
+    operator T&() { return get(); }
+
+};
+
+// copying reference
+template<class T>
+class copyref {
+    std::unique_ptr<T> inner;
+
+public:
+    copyref(const T& e) : inner(new T(e)) {};
+    copyref(const copyref& ref) : inner(new T(*ref.inner)) {};
+
+    const copyref& operator=(const copyref& ref) {
+        inner.reset(new T(*ref.inner));
+        return *this;
+    }
+    const copyref& operator=(T e) {
+        inner.reset(new T(e));
+        return *this;
+    }
+
+    T get() const { return *inner; }
+
+    operator T() { return get(); }
+
+};
+
 } // naemspace util
 } // naemspace borealis
 
