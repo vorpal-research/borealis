9f35ee82640e416a2acc80b08e5da1266ada0579
Mikhail Belyaev <devnull@localhost>
2012-12-19 17:35:06 +0400

    m

--- Util/unmacros.h
+++ Util/unmacros.h
@@ -6,16 +6,16 @@
  */
 
 // GUARDS ARE NOT USED FOR A REASON!
-//#ifndef UNMACROS_H_
-//#define UNMACROS_H_
+// #ifndef UNMACROS_H_
+// #define UNMACROS_H_
 
 #ifdef BOREALIS_MACROS_DEFINED
 #undef QUICK_RETURN
 #undef BOREALIS_MACROS_DEFINED
 #else
 #error "unmacros.h is included without corresponding macros.h include!"
-#endif //BOREALIS_MACROS_DEFINED
+#endif // BOREALIS_MACROS_DEFINED
 
 
 
-//#endif /* UNMACROS_H_ */
+// #endif /* UNMACROS_H_ */
