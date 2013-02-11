da3f68731e58ea16cb07c3000937ae9893043f5a
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-02-11 15:56:58 +0400

    Redesigned PassModilarizer

---
+++ Passes/PassModularizer.hpp
@@ -0,0 +1,98 @@
+/*
+ * PassModulizer.hpp
+ *
+ *  Created on: Feb 8, 2013
+ *      Author: belyaev
+ */
+
+#ifndef PASSMODULIZER_HPP_
+#define PASSMODULIZER_HPP_
+
+#include <llvm/Module.h>
+#include <llvm/Pass.h>
+#include <llvm/PassAnalysisSupport.h>
+
+#include <unordered_map>
+#include <utility>
+
+#include "Util/util.h"
+
+#include "Util/macros.h"
+
+namespace borealis {
+
+// Should be used only with [SubPass = ProxyFunctionPass<...>]
+//
+template<class SubPass>
+class PassModularizer : public llvm::ModulePass {
+
+    typedef std::unique_ptr<SubPass> subptr;
+
+    std::unordered_map<llvm::Function*, subptr> passes;
+
+    SubPass* getDefaultSubPass() const {
+        return passes.at(nullptr).get();
+    }
+
+    subptr createSubPass() {
+        return subptr(new SubPass(this));
+    }
+
+public:
+
+    static char ID;
+
+    PassModularizer() : llvm::ModulePass(ID) {
+        passes[nullptr] = createSubPass();
+    }
+
+    virtual bool runOnModule(llvm::Module &M) {
+        bool changed = false;
+
+        for (auto& F : M) {
+            // Do not run on declarations
+            if (F.isDeclaration()) continue;
+
+            subptr ptr(createSubPass());
+            changed |= ptr->runOnFunction(F);
+            passes[&F] = std::move(ptr);
+        }
+
+        return changed;
+    }
+
+    virtual void getAnalysisUsage(llvm::AnalysisUsage& AU) const {
+        getDefaultSubPass()->getAnalysisUsage(AU);
+    }
+
+    SubPass& getResultsForFunction(llvm::Function* F) {
+        if (borealis::util::containsKey(F, passes)) {
+            return *passes[F];
+        } else {
+            BYE_BYE(SubPass&, "Unknown function: " + F->getName());
+        }
+    }
+};
+
+template<class SubPass>
+char PassModularizer<SubPass>::ID;
+
+template<class SubPass>
+struct RegisterModularizedPass {
+    RegisterModularizedPass(
+            const char* PassArg,
+            const char* Name,
+            bool CFGOnly = false,
+            bool isAnalysis = false) {
+
+        static llvm::RegisterPass< PassModularizer<SubPass> >
+        MX(PassArg, Name, CFGOnly, isAnalysis);
+
+    }
+};
+
+} // namespace borealis
+
+#include "Util/unmacros.h"
+
+#endif /* PASSMODULIZER_HPP_ */
