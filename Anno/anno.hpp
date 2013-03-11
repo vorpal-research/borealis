c978d4b43ea66eee0e763f0868843dd2cf1df45f
Mikhail Belyaev <devnull@localhost>
2013-03-11 17:38:50 +0400

    m

--- Anno/anno.hpp
+++ Anno/anno.hpp
@@ -292,29 +292,25 @@ struct integer :
         sor< ifmust< seq< one< '0' >, one< 'x', 'X' > >, plus< xdigit > >, // hex
                 ifmust< one< '0' >, star< range< '0', '7' > > >,           // oct
                 seq< range< '1', '9' >, star< digit > >                    // dec
-        > {
-};
+        > {};
+
 // --//-- with action applied
 struct push_integer :
-        pad< ifapply< integer, push< long long > >, space > {
-};
+        pad< ifapply< integer, push< long long > >, space > {};
 
 // boolean grammar
 // must come BEFORE the variable grammar, as 'true' and 'false' are valid var names
 // boolean := true | false
 struct boolean :
-        seq< sor< pstring< 't', 'r', 'u', 'e' >, pstring< 'f', 'a', 'l', 's', 'e' > > > {
-};
+        seq< sor< pstring< 't', 'r', 'u', 'e' >, pstring< 'f', 'a', 'l', 's', 'e' > > > {};
 // --//-- with action applied
 struct push_boolean :
-        pad< ifapply< boolean, push< bool > >, space > {
-};
+        pad< ifapply< boolean, push< bool > >, space > {};
 
 // FP grammar
 // exponent := ('e'|'E') ('+'|'-') +(DIGIT)
 struct exponent :
-        ifmust< one< 'e', 'E' >, one< '+', '-' >, plus< digit > > {
-};
+        ifmust< one< 'e', 'E' >, one< '+', '-' >, plus< digit > > {};
 // floating := +(DIGIT) exponent                    // 1e+2
 // floating := *(DIGIT) '.' +(DIGIT) ?exponent      // .23
 // floating := +(DIGIT) '.' *(DIGIT) ?exponent      // 1.
@@ -332,278 +328,217 @@ struct floating :
         > {};
 // --//-- with action applied
 struct push_floating :
-        pad< ifapply< floating, push< double > >, space > {
-};
+        pad< ifapply< floating, push< double > >, space > {};
 
 // variable is just an identifier, can push it right away
 struct push_variable :
-        pad< ifapply< identifier, push< stdstring > >, space > {
-};
+        pad< ifapply< identifier, push< stdstring > >, space > {};
 
 // builtin is just an identifier prefixed with '\'
 struct push_builtin :
-        pad< ifapply< seq< one< '\\' >, identifier >, push< stdstring > >, space > {
-};
+        pad< ifapply< seq< one< '\\' >, identifier >, push< stdstring > >, space > {};
 
 // a primitive is a variable or a literal
 struct push_primitive :
-        sor< push_floating, push_integer, push_boolean, push_variable, push_builtin > {
-};
+        sor< push_floating, push_integer, push_boolean, push_variable, push_builtin > {};
 
 // smth surrounded by spaces
 template<typename Rule>
 struct literal :
-        pad< Rule, space > {
-};
+        pad< Rule, space > {};
 
 // a char surrounded by spaces
 template<int C>
 struct chpad :
-        literal< one< C > > {
-};
+        literal< one< C > > {};
+
 // a string surrounded by spaces
 template<int ...Chars>
 struct strpad :
-        literal< pstring< Chars... > > {
-};
+        literal< pstring< Chars... > > {};
 
 // opening paren
 struct read_open :
-        chpad< '(' > {
-};
+        chpad< '(' > {};
 // closing paren
 struct read_close :
-        chpad< ')' > {
-};
+        chpad< ')' > {};
 
 // forward decl for the whole expression
 struct read_expr;
 // an atom is a primitive or a parentified expr
 struct read_atom :
-        sor< push_primitive, seq< read_open, read_expr, read_close > > {
-};
+        sor< push_primitive, seq< read_open, read_expr, read_close > > {};
 
 // binary operation rule with oper sign being one char
 // eq to:
 //    bop := 'P' O with action A
 template<int P, typename O, typename A>
 struct read_bop :
-        ifapply< seq< chpad< P >, O >, op_baction< A > > {
-};
+        ifapply< seq< chpad< P >, O >, op_baction< A > > {};
 // generic binary operation rule
 // eq to:
 //    bop := Rule O with action A
 template<typename Rule, typename O, typename A>
-struct read_bop_m {
-};
+struct read_bop_m;
 template<typename O, typename A, int ...Chars>
 struct read_bop_m< pstring< Chars... >, O, A > :
-        ifapply< seq< strpad< Chars... >, O >, op_baction< A > > {
-};
+        ifapply< seq< strpad< Chars... >, O >, op_baction< A > > {};
 
 // unary operation rule
 // eq to:
 //    uop := 'P' O with action A
 template<int P, typename O, typename A>
 struct read_uop :
-        ifapply< seq< chpad< P >, O >, op_uaction< A > > {
-};
+        ifapply< seq< chpad< P >, O >, op_uaction< A > > {};
 
 // load := '*' atom
 struct read_load :
-        read_uop< '*', read_atom, load< expression_type > > {
-};
+        read_uop< '*', read_atom, load< expression_type > > {};
 // neg := '-' atom
 struct read_neg :
-        read_uop< '-', read_atom, negate< expression_type > > {
-};
+        read_uop< '-', read_atom, negate< expression_type > > {};
 // not := '!' atom
 struct read_not :
-        read_uop< '!', read_atom, logical_not< expression_type > > {
-};
+        read_uop< '!', read_atom, logical_not< expression_type > > {};
 // bnot := '~' atom
 struct read_bnot :
-        read_uop< '~', read_atom, bit_not< expression_type > > {
-};
+        read_uop< '~', read_atom, bit_not< expression_type > > {};
 
 // an unary op is just an atom, neg, not or bnot
 struct read_unary :
-        sor< read_load, read_neg, read_not, read_bnot, read_atom > {
-};
+        sor< read_load, read_neg, read_not, read_bnot, read_atom > {};
 
 // mul := '*' unary
 struct read_mul :
-        read_bop< '*', read_unary, multiplies< expression_type > > {
-};
+        read_bop< '*', read_unary, multiplies< expression_type > > {};
 // div := '/' unary
 struct read_div :
-        read_bop< '/', read_unary, divides< expression_type > > {
-};
+        read_bop< '/', read_unary, divides< expression_type > > {};
 // mod := '%' unary
 struct read_mod :
-        read_bop< '%', read_unary, modulus< expression_type > > {
-};
+        read_bop< '%', read_unary, modulus< expression_type > > {};
 // prod = unary *(mul | div | mod)
 struct read_prod :
-        seq< read_unary, star< sor< read_mul, read_div, read_mod > > > {
-};
+        seq< read_unary, star< sor< read_mul, read_div, read_mod > > > {};
 // add := '+' prod
 struct read_add :
-        read_bop< '+', read_prod, pluss< expression_type > > {
-};
+        read_bop< '+', read_prod, pluss< expression_type > > {};
 // sub := '-' prod
 struct read_sub :
-        read_bop< '-', read_prod, minus< expression_type > > {
-};
+        read_bop< '-', read_prod, minus< expression_type > > {};
 // sum := prod *(add | sub)
 struct read_sum :
-        seq< read_prod, star< sor< read_add, read_sub > > > {
-};
+        seq< read_prod, star< sor< read_add, read_sub > > > {};
 // lsh := '<<' sum
 struct read_lsh :
-        read_bop_m< pstring< '<', '<' >, read_sum, left_shift< expression_type > > {
-};
+        read_bop_m< pstring< '<', '<' >, read_sum, left_shift< expression_type > > {};
 // rsh := '>>' sum
 struct read_rsh :
-        read_bop_m< pstring< '>', '>' >, read_sum, right_shift< expression_type > > {
-};
+        read_bop_m< pstring< '>', '>' >, read_sum, right_shift< expression_type > > {};
 // shift := sum *(lsh | rsh)
 struct read_shift :
-        seq< read_sum, star< sor< read_lsh, read_rsh > > > {
-};
+        seq< read_sum, star< sor< read_lsh, read_rsh > > > {};
 
 // gt := '>' shift
 struct read_gt :
-        read_bop< '>', read_shift, greater< expression_type > > {
-};
+        read_bop< '>', read_shift, greater< expression_type > > {};
 // lt := '<' shift
 struct read_lt :
-        read_bop< '<', read_shift, less< expression_type > > {
-};
+        read_bop< '<', read_shift, less< expression_type > > {};
 // lt := '>=' shift
 struct read_ge :
-        read_bop_m< pstring< '>', '=' >, read_shift, greater_or_equal< expression_type > > {
-};
+        read_bop_m< pstring< '>', '=' >, read_shift, greater_or_equal< expression_type > > {};
 // lt := '<=' shift
 struct read_le :
-        read_bop_m< pstring< '<', '=' >, read_shift, less_or_equal< expression_type > > {
-};
+        read_bop_m< pstring< '<', '=' >, read_shift, less_or_equal< expression_type > > {};
 // inequality := shift *(gt | lt | ge | le)
 struct read_inequality :
-        seq< read_shift, star< sor< read_ge, read_gt, read_le, read_lt > > > {
-};
+        seq< read_shift, star< sor< read_ge, read_gt, read_le, read_lt > > > {};
 // eq = '==' inequality
 struct read_eq :
-        read_bop_m< pstring< '=', '=' >, read_inequality, equal< expression_type > > {
-};
+        read_bop_m< pstring< '=', '=' >, read_inequality, equal< expression_type > > {};
 // neq = '!=' inequality
 struct read_neq :
-        read_bop_m< pstring< '!', '=' >, read_inequality, nequal< expression_type > > {
-};
+        read_bop_m< pstring< '!', '=' >, read_inequality, nequal< expression_type > > {};
 // equality := inequality *(eq | neq)
 struct read_equality :
-        seq< read_inequality, star< sor< read_eq, read_neq > > > {
-};
+        seq< read_inequality, star< sor< read_eq, read_neq > > > {};
 
 // bitand := '&' equality
 struct read_bitand :
-        read_bop< '&', read_equality, bit_and< expression_type > > {
-};
+        read_bop< '&', read_equality, bit_and< expression_type > > {};
 // bitand2 := equality *(bitand)
 struct read_bitand2 :
-        seq< read_equality, star< read_bitand > > {
-};
+        seq< read_equality, star< read_bitand > > {};
 
 // bitxor := '^' bitand2
 struct read_bitxor :
-        read_bop< '^', read_bitand2, bit_xor< expression_type > > {
-};
+        read_bop< '^', read_bitand2, bit_xor< expression_type > > {};
 // bitxor2 := bitand2 *(bitxor)
 struct read_bitxor2 :
-        seq< read_bitand2, star< read_bitxor > > {
-};
+        seq< read_bitand2, star< read_bitxor > > {};
 
 // bitor := '|' bitxor2
 struct read_bitor :
-        read_bop< '|', read_bitxor2, bit_or< expression_type > > {
-};
+        read_bop< '|', read_bitxor2, bit_or< expression_type > > {};
 // bitor2 := bitxor2 *(bitor)
 struct read_bitor2 :
-        seq< read_bitxor2, star< read_bitor > > {
-};
+        seq< read_bitxor2, star< read_bitor > > {};
 
 // land := '&&' bitor2
 struct read_land :
-        read_bop_m< pstring< '&', '&' >, read_bitor2, logical_and< expression_type > > {
-};
+        read_bop_m< pstring< '&', '&' >, read_bitor2, logical_and< expression_type > > {};
 // land2 := bitor2 *(land)
 struct read_land2 :
-        seq< read_bitor2, star< read_land > > {
-};
+        seq< read_bitor2, star< read_land > > {};
 
 // lor := '||' land2
 struct read_lor :
-        read_bop_m< pstring< '|', '|' >, read_land2, logical_or< expression_type > > {
-};
+        read_bop_m< pstring< '|', '|' >, read_land2, logical_or< expression_type > > {};
 // lor2 := land2 *(lor)
 struct read_lor2 :
-        seq< read_land2, star< read_lor > > {
-};
+        seq< read_land2, star< read_lor > > {};
 // lor2 is the root of all expressions (as we don't consider even less bound ones)
 // XXX: do we need to implement ternary operator (:?)
 // if we do, it will be right below lor2
 struct read_expr :
-        read_lor2 {
-};
+        read_lor2 {};
 
 // mask := +(ALPHA) *(DIGIT)
 struct mask :
-        seq< plus< alpha >, star< digit > > {
-};
+        seq< plus< alpha >, star< digit > > {};
 // mask is just an identifier, can push it right away
 struct push_mask :
-        pad< ifapply< mask, mask_push >, space > {
-};
+        pad< ifapply< mask, mask_push >, space > {};
 // masks := mask *(',' mask)
 struct masks :
-        seq< push_mask, star< seq< chpad< ',' >, push_mask > > > {
-};
+        seq< push_mask, star< seq< chpad< ',' >, push_mask > > > {};
 
 template<int ...Name>
 struct keyword :
-        ifapply< strpad< Name... >, ask_keyword > {
-};
+        ifapply< strpad< Name... >, ask_keyword > {};
 
 struct requires_c :
-        ifapply< ifmust< keyword< 'r', 'e', 'q', 'u', 'i', 'r', 'e', 's' >, read_expr >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 'r', 'e', 'q', 'u', 'i', 'r', 'e', 's' >, read_expr >, ask_arguments > {};
 struct ensures_c :
-        ifapply< ifmust< keyword< 'e', 'n', 's', 'u', 'r', 'e', 's' >, read_expr >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 'e', 'n', 's', 'u', 'r', 'e', 's' >, read_expr >, ask_arguments > {};
 struct assigns_c :
-        ifapply< ifmust< keyword< 'a', 's', 's', 'i', 'g', 'n', 's' >, read_expr >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 'a', 's', 's', 'i', 'g', 'n', 's' >, read_expr >, ask_arguments > {};
 struct assert_c :
-        ifapply< ifmust< keyword< 'a', 's', 's', 'e', 'r', 't' >, read_expr >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 'a', 's', 's', 'e', 'r', 't' >, read_expr >, ask_arguments > {};
 struct skip_c :
-        ifapply< keyword< 's', 'k', 'i', 'p' >, ask_arguments > {
-};
+        ifapply< keyword< 's', 'k', 'i', 'p' >, ask_arguments > {};
 struct ignore_c :
-        ifapply< keyword< 'i', 'g', 'n', 'o', 'r', 'e' >, ask_arguments > {
-};
+        ifapply< keyword< 'i', 'g', 'n', 'o', 'r', 'e' >, ask_arguments > {};
 struct stack_depth_c :
-        ifapply< ifmust< keyword< 's', 't', 'a', 'c', 'k', '-', 'd', 'e', 'p', 't', 'h' >, push_integer >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 's', 't', 'a', 'c', 'k', '-', 'd', 'e', 'p', 't', 'h' >, push_integer >, ask_arguments > {};
 struct unroll_c :
-        ifapply< ifmust< keyword< 'u', 'n', 'r', 'o', 'l', 'l' >, push_integer >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 'u', 'n', 'r', 'o', 'l', 'l' >, push_integer >, ask_arguments > {};
 struct mask_c :
-        ifapply< ifmust< keyword< 'm', 'a', 's', 'k' >, masks >, ask_arguments > {
-};
+        ifapply< ifmust< keyword< 'm', 'a', 's', 'k' >, masks >, ask_arguments > {};
 struct endmask_c :
-        ifapply< keyword< 'e', 'n', 'd', 'm', 'a', 's', 'k' >, ask_arguments > {
-};
+        ifapply< keyword< 'e', 'n', 'd', 'm', 'a', 's', 'k' >, ask_arguments > {};
 
 struct commands :
         sor<
@@ -617,8 +552,7 @@ struct commands :
             unroll_c,
             mask_c,
             endmask_c
-        > {
-};
+        > {};
 
 struct acom :
         seq< one< '@' >, commands > {};
@@ -628,8 +562,7 @@ struct annotated_commentary :
         sor< seq< pstring< '/', '/' >, plus< literal< acom_c > > >, seq< pstring< '/', '*' >, plus< literal< acom_c > >, pstring< '*', '/' > > > {};
 
 struct read_calc :
-        seq< commands, space_until_eof > {
-};
+        seq< commands, space_until_eof > {};
 
 template<class Location = pegtl::ascii_location>
 std::vector< command_type > parse_command(const std::string& command) {
@@ -641,7 +574,7 @@ std::vector< command_type > parse_command(const std::string& command) {
 
     if(cp.success) {
         return std::move(commands);
-    }else {
+    } else {
         return std::vector< command_type >();
     }
 }
