bdb4ba925bb31d78769d66ed423ca89e3a3a73a0
Mikhail Belyaev <devnull@localhost>
2013-03-28 20:09:05 +0400

    m

---
+++ Annotation/AssumeAnnotation.cpp
@@ -0,0 +1,8 @@
+/*
+ * AssumeAnnotation.cpp
+ *
+ *  Created on: Jan 22, 2013
+ *      Author: belyaev
+ */
+
+#include "Annotation/AssumeAnnotation.h"
---
+++ Annotation/AssumeAnnotation.h
@@ -0,0 +1,45 @@
+/*
+ * AssumeAnnotation.h
+ *
+ *  Created on: Jan 22, 2013
+ *      Author: belyaev
+ */
+
+#ifndef ASSUMEANNOTATION_H_
+#define ASSUMEANNOTATION_H_
+
+#include "Annotation/AnnotationNames.hpp"
+#include "Annotation/LogicAnnotation.h"
+
+namespace borealis {
+
+class AssumeAnnotation: public LogicAnnotation {
+    typedef AssumeAnnotation self;
+
+public:
+    AssumeAnnotation(const Locus& locus, Term::Ptr term):
+        LogicAnnotation(type_id(*this), locus, AnnotationNames<self>::name(), term) {}
+    virtual ~AssumeAnnotation(){}
+
+    static bool classof(const Annotation* a) {
+        if (auto* la = llvm::dyn_cast_or_null<LogicAnnotation>(a)) {
+            return la->getTypeId() == type_id<self>();
+        } else return false;
+    }
+
+    static bool classof(const self* /* p */) {
+        return true;
+    }
+
+    static Annotation::Ptr fromTerms(const Locus& locus, const std::vector<Term::Ptr>& terms) {
+        return Annotation::Ptr{ new self(locus, terms.front()) };
+    }
+
+    Annotation::Ptr clone(Term::Ptr newTerm) const {
+        return Annotation::Ptr{ new self(locus, newTerm) };
+    }
+};
+
+} /* namespace borealis */
+
+#endif /* ASSUMEANNOTATION_H_ */
