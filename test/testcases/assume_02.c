1d239d98ba68f1091851c4d95c3ee603f8ab1cb4
Mikhail Belyaev <devnull@localhost>
2013-04-12 18:13:02 +0400

    added forgotten contract testcases

---
+++ test/testcases/assume_02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+struct el {
+    double d;
+    char c;
+};
+
+void do_smth(struct el**);
+
+int main(void) {
+    struct el* ptr;
+    do_smth(&ptr);
+
+    ptr = (struct el*)malloc(2 * sizeof(struct el));
+    // @assume ptr != 0
+
+    ptr[0].c = 1;
+    ptr[1].c = 2;
+
+    return rand() * ptr[0].c;
+}
