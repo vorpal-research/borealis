fd524e17e6ae185e63e0c4107e20237b34bf3c31
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-04-15 20:46:12 +0400

    Better stream output operators for std::vector and std::set

--- Util/streams.hpp
+++ Util/streams.hpp
@@ -158,16 +158,18 @@ std::ostream& output_using_llvm(std::ostream& ost, const T& val) {
 namespace std {
 
 namespace impl_ {
-// Custom output routines and such...
-const auto VECTOR_LEFT_BRACE = "[";
-const auto VECTOR_RIGHT_BRACE = "]";
-const auto SET_LEFT_BRACE = "(";
-const auto SET_RIGHT_BRACE = ")";
+// Custom output stuff and such...
+const auto VECTOR_LEFT_BRACE = '[';
+const auto VECTOR_RIGHT_BRACE = ']';
+const auto SET_LEFT_BRACE = '(';
+const auto SET_RIGHT_BRACE = ')';
 const auto ELEMENT_DELIMITER = ", ";
-const auto TUPLE_LEFT_BRACE = "{";
-const auto TUPLE_RIGHT_BRACE = "}";
+const auto TUPLE_LEFT_BRACE = '{';
+const auto TUPLE_RIGHT_BRACE = '}';
 const auto NULL_REPR = "<NULL>";
 } // namespace impl_
+
+
 
 template<typename T, typename U, typename Streamer>
 Streamer& operator <<(Streamer& s, const std::pair<T, U>& pp) {
@@ -182,112 +184,72 @@ Streamer& operator <<(Streamer& s, const std::pair<T, U>& pp) {
     return s;
 }
 
+
+
+template<typename Elem, typename SFINAE = void>
+struct elemPrettyPrinter;
+
+#include "Util/macros.h"
+
+template< typename Elem >
+struct elemPrettyPrinter<Elem, GUARD(std::is_pointer<Elem>::value)> {
+    template<class Streamer>
+    static Streamer& doit(Streamer& s, const Elem& e) {
+        // this is generally fucked up...
+        return static_cast<Streamer&>(e == nullptr ? s << impl_::NULL_REPR : s << *e);
+    }
+};
+
+template< typename Elem >
+struct elemPrettyPrinter<Elem, GUARD(!std::is_pointer<Elem>::value)> {
+    template<class Streamer>
+    static Streamer& doit(Streamer& s, const Elem& e) {
+        // this is generally fucked up...
+        return static_cast<Streamer&>(s << e);
+    }
+};
+
+#include "Util/unmacros.h"
+
+
+
+template< class Container >
+struct containerPrettyPrinter {
+    template<char LB, char RB, class Streamer>
+    static Streamer& doit(Streamer& s, const Container& con) {
+        typedef typename Container::const_iterator ConstIter;
+        typedef typename Container::value_type Elem;
+        typedef elemPrettyPrinter<Elem> epp;
+
+        using namespace std::impl_;
+        using borealis::util::view;
+
+        s << LB;
+        if (!con.empty()) {
+            ConstIter iter = con.begin();
+            const Elem& el = *iter++;
+            epp::doit(s, el);
+            for (const auto& e : view(iter, con.end())) {
+                s << ELEMENT_DELIMITER;
+                epp::doit(s, e);
+            }
+        }
+        s << RB;
+
+        return s;
+    }
+};
+
 template<typename T, typename Streamer>
 Streamer& operator <<(Streamer& s, const std::vector<T>& vec) {
-    typedef typename std::vector<T>::const_iterator ConstIter;
-
-    using namespace std::impl_;
-    using borealis::util::view;
-
-    s << VECTOR_LEFT_BRACE;
-    if (!vec.empty()) {
-        ConstIter iter = vec.begin();
-        const T el = *iter++;
-        s << el;
-        for (auto& e : view(iter, vec.end())) {
-            s << ELEMENT_DELIMITER << e;
-        }
-    }
-    s << VECTOR_RIGHT_BRACE;
-
-    return s;
+    typedef std::vector<T> Container;
+    return containerPrettyPrinter< Container >::template doit<impl_::VECTOR_LEFT_BRACE, impl_::VECTOR_RIGHT_BRACE>(s, vec);
 }
 
 template<typename T, typename Streamer>
-Streamer& operator <<(Streamer& s, const std::set<T>& set) {
-    typedef typename std::set<T>::const_iterator ConstIter;
-
-    using namespace std::impl_;
-    using borealis::util::view;
-
-    s << SET_LEFT_BRACE;
-    if (!set.empty()) {
-        ConstIter iter = set.begin();
-        const T el = *iter++;
-        s << el;
-        for (auto& e : view(iter, set.end())) {
-            s << ELEMENT_DELIMITER << e;
-        }
-    }
-    s << SET_RIGHT_BRACE;
-
-    return s;
-}
-
-template<typename T, typename Streamer>
-Streamer& operator <<(Streamer& s, const std::vector<T*>& vec) {
-    typedef typename std::vector<T*>::const_iterator ConstIter;
-
-    using namespace std::impl_;
-    using borealis::util::view;
-
-    s << VECTOR_LEFT_BRACE;
-    if (!vec.empty()) {
-        ConstIter iter = vec.begin();
-        const T* el = *iter++;
-        (el == NULL ? s << NULL_REPR : s << *el);
-        for (auto& e : view(iter, vec.end())) {
-            s << ELEMENT_DELIMITER;
-            (e == NULL ? s << NULL_REPR : s << *e);
-        }
-    }
-    s << VECTOR_RIGHT_BRACE;
-
-    return s;
-}
-
-template<typename Streamer>
-Streamer& operator <<(Streamer& s, const std::vector<const char*>& vec) {
-    typedef typename std::vector<const char*>::const_iterator ConstIter;
-
-    using namespace std::impl_;
-    using borealis::util::view;
-
-    s << VECTOR_LEFT_BRACE;
-    if (!vec.empty()) {
-        ConstIter iter = vec.begin();
-        auto el = *iter++;
-        (el == NULL ? s << NULL_REPR : s << el);
-        for (auto& e : view(iter, vec.end())) {
-            s << ELEMENT_DELIMITER;
-            (e == NULL ? s << NULL_REPR : s << e);
-        }
-    }
-    s << VECTOR_RIGHT_BRACE;
-
-    return s;
-}
-
-template<typename T, typename Streamer>
-Streamer& operator <<(Streamer& s, const std::set<T*>& set) {
-    typedef typename std::set<T*>::const_iterator ConstIter;
-
-    using namespace std::impl_;
-    using borealis::util::view;
-
-    s << SET_LEFT_BRACE;
-    if (!set.empty()) {
-        ConstIter iter = set.begin();
-        const T* el = *iter++;
-        (el == NULL ? s << NULL_REPR : s << *el);
-        for (auto& e : view(iter, set.end())) {
-            s << ELEMENT_DELIMITER;
-            (e == NULL ? s << NULL_REPR : s << *e);
-        }
-    }
-    s << SET_RIGHT_BRACE;
-
-    return s;
+Streamer& operator <<(Streamer& s, const std::set<T>& vec) {
+    typedef std::set<T> Container;
+    return containerPrettyPrinter< Container >::template doit<impl_::SET_LEFT_BRACE, impl_::SET_RIGHT_BRACE>(s, vec);
 }
 
 } // namespace std
