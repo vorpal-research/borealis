b728942b4085df192e9d2f940e0d4203cae9f140
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-04-16 14:24:05 +0400

    Added NECLA test suite (not used atm)

---
+++ test/testcases/necla/ex19.c
@@ -0,0 +1,22 @@
+
+int main(int i, int j){
+
+   int x,y;
+   int a[1];
+   x=i;
+   y=j;
+
+   while (x!=0) {
+      x--;
+      y--;
+   }
+
+   if (i==j){
+      ASSERT( y == 0);
+
+      
+   }
+   
+   
+   return 1;
+}
---
+++ test/testcases/necla/inf1.c
@@ -0,0 +1,29 @@
+int main(int a, int b, int c){
+	int flag1,flag2;
+	
+	if (a > b) {
+		flag1 = 1;
+	} else {
+		flag1 =0;
+	}
+
+	if (b > c){
+		flag2 =1;
+	} else {
+		flag2 = 0;
+	}
+
+	if (flag1 == 1){
+		if (flag2 == 1){
+			ASSERT( a >= c);
+		}
+	}
+
+	if ( flag2 -flag1 <= 0 ) {
+		if (flag1 + flag2 < 1){
+			ASSERT(a < c);
+		}
+	}
+	return 1;
+}
+
