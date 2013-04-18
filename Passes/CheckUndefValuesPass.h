e0ebbed31797df5c458a8e7097cb7e07747324ad
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-04-18 16:50:33 +0400

    Created NDF-01 checker that finds all uses of undef values

---
+++ Passes/CheckUndefValuesPass.h
@@ -0,0 +1,48 @@
+/*
+ * CheckUndefValuesPass.h
+ *
+ *  Created on: Apr 18, 2013
+ *      Author: ice-phoenix
+ */
+
+#ifndef CHECKUNDEFVALUESPASS_H_
+#define CHECKUNDEFVALUESPASS_H_
+
+#include <llvm/Pass.h>
+
+#include "Logging/logger.hpp"
+#include "Passes/DefectManager.h"
+#include "Util/passes.hpp"
+
+namespace borealis {
+
+class CheckUndefValuesPass :
+        public borealis::ProxyFunctionPass,
+        public borealis::logging::ClassLevelLogging<CheckUndefValuesPass>,
+        public ShouldBeModularized {
+
+    friend class UndefInstVisitor;
+
+public:
+
+    static char ID;
+
+#include "Util/macros.h"
+    static constexpr auto loggerDomain() QUICK_RETURN("check-undefs")
+#include "Util/unmacros.h"
+
+    CheckUndefValuesPass();
+    CheckUndefValuesPass(llvm::Pass*);
+    virtual bool runOnFunction(llvm::Function& F);
+    virtual void getAnalysisUsage(llvm::AnalysisUsage& AU) const;
+    virtual ~CheckUndefValuesPass();
+
+private:
+
+    DefectManager* DM;
+
+};
+
+} /* namespace borealis */
+
+#endif /* CHECKUNDEFVALUESPASS_H_ */
