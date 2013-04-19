180bdc5e826e6e02b3cb2ecb6a5748b89fa769d6
Mikhail Belyaev <devnull@localhost>
2013-04-19 19:03:47 +0400

    More NECLA test tweaking

--- test/testcases/necla/defines.h
+++ test/testcases/necla/defines.h
@@ -1,7 +1,12 @@
+#include <stdlib.h>
+
 void borealis_assert(int);
 void borealis_assume(int);
 int borealis_nondet();
 
-#define ASSERT borealis_assert
-#define ASSUME borealis_assume
+// this is generally fucked up
+#define main __main
+
+#define ASSERT(X) borealis_assert((int)(X))
+#define ASSUME(X) borealis_assume((int)(X))
 #define __NONDET__ borealis_nondet
