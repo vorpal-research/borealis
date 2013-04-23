460cb45f53ce823021066ff003c5b204406624b3
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-04-23 13:46:53 +0400

    Even more...

--- test/testcases/necla/ex47.c
+++ test/testcases/necla/ex47.c
@@ -12,17 +12,21 @@ int main(int h, int w, int k){
     ASSUME(h > 0);
     ASSUME(w > 0);
     ASSUME(k > 0 && k < h * w);
-   
-	lut = (DIST**) malloc(sizeof(DIST*) * h);
-	lut[0] = (DIST*) malloc(sizeof(DIST) * h * w);
-	for (y=0; y<h; y++)
-		lut[y] = lut[0] + w*y;
 
-	array = lut[0];
-    // ASSERT(array[k] == lut[k/w][k%w]);
-    
+    lut = (DIST**) malloc(sizeof(DIST*) * h);
+    ASSUME(lut);
+
+    lut[0] = (DIST*) malloc(sizeof(DIST) * h * w);
+    ASSUME(lut[0]);
+
+    for (y=0; y<h; y++)
+        lut[y] = lut[0] + w*y;
+
+    array = lut[0];
+    ASSERT(array[k] == lut[k/w][k%w]);
+
     free(lut[0]);
     free(lut);
-    
+
     return 1;
 }
