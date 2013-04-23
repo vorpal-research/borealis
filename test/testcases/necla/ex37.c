ac6ec77af3080c1b3ce83e41fb7ca655efea16cc
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-04-23 13:37:14 +0400

    Yup, more test fixes

--- test/testcases/necla/ex37.c
+++ test/testcases/necla/ex37.c
@@ -1,24 +1,22 @@
 #include "defines.h"
 #include <stdlib.h>
+
 typedef struct foo{
-   
    int x;
    int z;
-
 } foo_t;
 
-
-int main(int n){
-   foo_t * a;
-   int * b;
+int main(int n) {
+   foo_t* a;
+   int* b;
    ASSUME(n > 0);
    
    a = (foo_t*) malloc(n * sizeof(foo_t));
+   ASSUME(a);
 
-   b = (int *) a; /*-- down casting. Length(b) = 2 * Length(a) --*/
+   b = (int*) a; /*-- down casting. Length(b) = 2 * Length(a) --*/
 
-   b[2*n -1 ] = '\0';
-   
+   b[2*n-1] = '\0';
 
    return 1;
 }
--- test/testcases/necla/ex8.c
+++ test/testcases/necla/ex8.c
@@ -1,17 +1,17 @@
 #include "defines.h"
 
-int main(){
-
-   int * a;
+int main() {
+   int* a;
    int k = __NONDET__();
    int i;
-   if ( k <= 0 ) return -1;
+   if (k <= 0) return -1;
    
-   a= malloc( k * sizeof(int));
-   
-   for (i =0 ; i != k; i++)
-      if (a[i]) return 1;
+   a = malloc(k * sizeof(int));
+   ASSUME(a);
+
+   for (i = 0; i != k; i++)
+      if (a[i])
+      	return 1;
 
    return 0;
-
 }
