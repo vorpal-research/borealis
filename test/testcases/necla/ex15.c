2b610f67bc24c4b128f9d5129e4d81c7c36eb034
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-06-03 14:31:21 +0400

    m in tests

--- test/testcases/necla/ex15.c
+++ test/testcases/necla/ex15.c
@@ -1,35 +1,38 @@
 #include "defines.h"
-typedef struct{
-   int * a;
-   int * b;
-   int * c;
+
+typedef struct {
+   int* a;
+   int* b;
+   int* c;
 } ptrStruct;
+
 int i;
 
-int foo(ptrStruct * ptr){
-   *(ptr -> a)=i++;
-   *(ptr -> b)=i++;
+// @requires ptr != 0
+int foo(ptrStruct* ptr) {
+   *(ptr->a) = i++;
+   *(ptr->b) = i++;
    return 1;
 }
 
-int main(){
+int main() {
    int x,y,z;
    int a,b,c;
-   i=0;
    ptrStruct A,B;
+
+   i = 0;
+
    A.a = &x;
    A.b = &y;
    A.c = &z;
-
    foo(&A);
 
    B.a = &a;
    B.b = &b;
    B.c = &c;
-
    foo(&B);
    
-   ASSERT( *(B.b) >= *(A.a));
-   return -1;
+   ASSERT( *(B.b) >= *(A.a) );
 
+   return -1;
 }
