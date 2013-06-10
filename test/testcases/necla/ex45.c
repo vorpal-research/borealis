556a303f14c5ee5705b4180267a2782760b8b630
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-06-10 14:51:06 +0400

    one-for-all + no unreachable check by default + more enabled long tests

--- test/testcases/necla/ex45.c
+++ test/testcases/necla/ex45.c
@@ -2,42 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-// parse the string based on the token char c.
-char **parse_token(char *str, char c)
-{
+// @requires str != 0
+char** parse_token(char* str, char c) {
    int len = strlen(str);
    int loc = 0;
    int num = 0;
    int i;
-   char **res;
-   
-   for (i = 0; i < len; i++)
-      if (str[i] == c)
-            num++;
+   char** res;
 
-   res = (char **) malloc(num * sizeof (char*));
-   for (i = 0; i < len; i++)
-   {
+   for (i = 0; i < len; ++i)
+      if (str[i] == c)
+         num++;
+
+   res = (char**) malloc(num * sizeof(char*));
+   ASSUME(res);
+
+   for (i = 0; i < len; ++i) {
       if (str[i] == c) {
-        char *temp_str = (char *) calloc (i-loc+1, 1); // the last char is 0
-        memcpy(temp_str, str+loc, i-loc);
-        res[num++] = temp_str;
-        loc = i+1;
+         char* temp_str = (char*) calloc(i-loc+1, 1); // the last char is 0
+         memcpy(temp_str, str + loc, i - loc);
+         res[num++] = temp_str;
+         loc = i + 1;
       }
-             
    }
 
    return res;
-   
 }
 
-int main(char *str, char c){
+int main(char* str, char c) {
 
-   char **tokens;
-  
+   char** tokens;
+
+   ASSUME(str);
    ASSUME(strlen(str) > 0);
-   
+
    tokens = parse_token(str, c);
-   
+
    return 1;
 }
