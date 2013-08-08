3a50abe955269f34b15f75e36233bc2dbefba198
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2013-08-08 13:24:45 +0400

    Code review + m + added location-summary to post-passes

--- Passes/Location/LocationSummaryPass.cpp
+++ Passes/Location/LocationSummaryPass.cpp
@@ -5,9 +5,10 @@
  *      Author: snowball
  */
 
-#include "LocationSummaryPass.h"
 #include "Passes/Location/LocationManager.h"
+#include "Passes/Location/LocationSummaryPass.h"
 #include "Passes/Tracker/SourceLocationTracker.h"
+
 #include "Util/passes.hpp"
 
 namespace borealis {
@@ -16,26 +17,23 @@ LocationSummaryPass::LocationSummaryPass(): ModulePass(ID) {}
 
 void LocationSummaryPass::getAnalysisUsage(llvm::AnalysisUsage& AU) const {
     AU.setPreservesAll();
-    AUX<LocationManager>::addRequiredTransitive(AU);
     AUX<SourceLocationTracker>::addRequiredTransitive(AU);
+    AUX<LocationManager>::addRequiredTransitive(AU);
 }
 
 bool LocationSummaryPass::runOnModule(llvm::Module&) {
     auto& slt = GetAnalysis<SourceLocationTracker>::doit(this);
     auto& lm = GetAnalysis<LocationManager>::doit(this);
 
-    infos() << "Visited locations:\n";
-    for (const auto value : lm.getLocations()) {
-        infos() << slt.getFilenameFor(value) << ' '
-                << slt.getLineFor(value) << ' '
-                << slt.getColumnFor(value) << '\n';
+    infos() << "Visited locations:" << endl;
+    for (const auto& value : lm.getLocations()) {
+        infos() << slt.getLocFor(value) << endl;
     }
     return false;
 }
 
 char LocationSummaryPass::ID;
 static RegisterPass<LocationSummaryPass>
-X("location-summary", "Pass that outputs locations visited by other passes");
+X("location-summary", "Pass that outputs visited locations");
 
 } /* namespace borealis */
-
