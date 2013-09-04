8e1509302353059a0b9741b446ac2cb1c102aa36
Polovtcev Alexander <sashapolo239@gmail.com>
2013-09-04 16:32:15 +0400

    added shitty script for downloading and compiling projects from github

---
+++ git_maker.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "usage: $0 <wrapper folder path> <github repo address>"
+    exit 1
+fi
+
+git clone $2
+
+dir=${2##*/}
+wrapper_prefix=$(readlink -f $1)
+
+cd $dir
+ln -fs "$wrapper_prefix/wrapper"
+ln -fs "$wrapper_prefix/wrapper.conf"
+ln -fs "$wrapper_prefix/log.ini"
+
+make CC=./wrapper
