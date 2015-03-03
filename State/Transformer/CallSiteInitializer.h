bfc6c30e3404e144680fb0a4efc115d28db9b584
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2015-03-03 17:57:02 +0300

    m in CallSiteInitializer

--- State/Transformer/CallSiteInitializer.h
+++ State/Transformer/CallSiteInitializer.h
@@ -23,83 +23,21 @@ namespace borealis {
 
 class CallSiteInitializer : public borealis::Transformer<CallSiteInitializer> {
 
-    typedef borealis::Transformer<CallSiteInitializer> Base;
+    using Base = borealis::Transformer<CallSiteInitializer>;
 
 public:
 
-    CallSiteInitializer(
-            const llvm::CallInst& CI,
-            FactoryNest FN) : Base(FN) {
+    CallSiteInitializer(const llvm::CallInst& CI, FactoryNest FN);
 
-        using borealis::util::toString;
+    Predicate::Ptr transformPredicate(Predicate::Ptr p);
 
-        returnValue = &CI;
-
-        if (auto* calledFunc = CI.getCalledFunction()) {
-            int argNum = calledFunc->arg_size();
-            for (int argIdx = 0; argIdx < argNum; ++argIdx) {
-                callSiteArguments[argIdx] = CI.getArgOperand(argIdx);
-            }
-        }
-
-        auto* callerFunc = CI.getParent()->getParent();
-        auto* callerInst = &CI;
-
-        auto callerFuncName = callerFunc && callerFunc->hasName()
-                              ? callerFunc->getName().str()
-                              : toString(callerFunc);
-        auto callerInstName = callerInst && callerInst->hasName()
-                              ? callerInst->getName().str()
-                              : toString(callerInst);
-
-        prefix = callerFuncName + "." + callerInstName + ".";
-    }
-
-    Predicate::Ptr transformPredicate(Predicate::Ptr p) {
-        switch(p->getType()) {
-        case PredicateType::ENSURES:
-            return Predicate::Ptr(
-                p->clone()->setType(PredicateType::STATE)
-            );
-        default:
-            return p;
-        }
-    }
-
-    Term::Ptr transformArgumentTerm(ArgumentTermPtr t) {
-        auto argIdx = t->getIdx();
-
-        ASSERT(callSiteArguments.count(argIdx) > 0,
-               "Cannot find an actual function argument at call site");
-
-        auto* actual = callSiteArguments.at(argIdx);
-
-        switch (t->getKind()) {
-        case ArgumentKind::STRING: {
-            auto argAsString = getAsCompileTimeString(actual);
-            ASSERT(!argAsString.empty(),
-                   "Non-string actual argument for ArgumentKind::STRING");
-            return FN.Term->getOpaqueConstantTerm(*argAsString.get());
-        }
-        default: {
-            return FN.Term->getValueTerm(actual);
-        }
-        }
-    }
-
-    Term::Ptr transformReturnValueTerm(ReturnValueTermPtr) {
-        return FN.Term->getValueTerm(returnValue);
-    }
-
-    Term::Ptr transformValueTerm(ValueTermPtr t) {
-        if (t->isGlobal()) return t;
-
-        return t->withNewName(prefix + t->getName());
-    }
+    Term::Ptr transformArgumentTerm(ArgumentTermPtr t);
+    Term::Ptr transformReturnValueTerm(ReturnValueTermPtr);
+    Term::Ptr transformValueTerm(ValueTermPtr t);
 
 private:
 
-    typedef std::unordered_map<unsigned int, const llvm::Value*> CallSiteArguments;
+    using CallSiteArguments = std::unordered_map<unsigned int, const llvm::Value*>;
 
     const llvm::Value* returnValue;
     CallSiteArguments callSiteArguments;
