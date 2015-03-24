3bf2d7040da9e78ed60654418ce5d27e85fcd335
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2015-03-24 14:32:53 +0300

    Reworked GlobalVariableLifting pass

--- Passes/Transform/GlobalVariableLifting.cpp
+++ Passes/Transform/GlobalVariableLifting.cpp
@@ -4,145 +4,159 @@
  *  Created on: 23 марта 2015 г.
  *      Author: abdullin
  */
-#include "GlobalVariableLifting.h"
-
 
 #include <llvm/IR/GlobalVariable.h>
 #include <llvm/IR/InstIterator.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/Transforms/Utils/BasicBlockUtils.h>
-#include <llvm/Support/raw_ostream.h>
 
 #include <unordered_map>
 #include <unordered_set>
 
+#include "Passes/Transform/GlobalVariableLifting.h"
+#include "Util/util.h"
 
-namespace borealis{
+namespace borealis {
 
-bool GlobalVariableLifting::runOnFunction(llvm::Function& F){
-    string_value_map globals;
-    std::unordered_map<llvm::BasicBlock*, string_value_map> locals_begin, locals_end;
-    std::unordered_set<llvm::Value*> delete_instructions;
+GlobalVariableLifting::GlobalVariableLifting() : llvm::FunctionPass(ID) {}
 
-    // find all global variables used by function
-    for (auto&& i = inst_begin(F); i != inst_end(F); ++i) {
-        for (auto&& op : i->operand_values()) {
-            if (llvm::isa<llvm::GlobalVariable>(op)) {
-            	if (!op->getType()->getPointerElementType()->isArrayTy())
-                	globals[op->getName()] = op;
-            }
+void GlobalVariableLifting::init() {
+    globals.clear();
+    locals_begin.clear();
+    locals_end.clear();
+    deleted_instructions.clear();
+}
+
+void GlobalVariableLifting::collectGlobals(llvm::Function& F) {
+    for (auto&& inst = llvm::inst_begin(F); inst != llvm::inst_end(F); ++inst) {
+        for (auto&& op : inst->operand_values()) {
+            if (llvm::isa<llvm::GlobalVariable>(op) &&
+                not op->getType()->getPointerElementType()->isArrayTy())
+                    globals[op->getName()] = op;
         }
     }
+}
 
-    for (auto&& it = F.begin(); it != F.end(); ++it) {
-        auto* bb = &*it;
-        auto&& j = bb->begin();
+void GlobalVariableLifting::setupBasicBlock(llvm::BasicBlock* BB) {
+    auto&& bb_locals_begin = locals_begin[BB];
+    auto&& bb_locals_end = locals_end[BB];
 
-        // insert loads at the start of current BB
-        for (auto&& g : globals) {
-            auto&& gName = g.first();
-            auto&& gValue = g.second;
-            auto* load = new llvm::LoadInst(gValue, gName + LOCAL_POSTFIX, &bb->front());
-            locals_begin[bb][gName] = locals_end[bb][gName] = load;
-        }
+    auto&& inst = BB->begin();
 
-        //replase globals to locals in current BB
-        for ( ; j != bb->end(); ++j) {
-            for (auto&& op : j->operand_values()) {
-                if (llvm::isa<llvm::GlobalVariable>(op)) {
-                   if (auto* load = llvm::dyn_cast<llvm::LoadInst>(j)) {
-                        j->replaceAllUsesWith(locals_end[bb][op->getName()]);
-                        delete_instructions.insert(j);
-                    } else if (auto* store = llvm::dyn_cast<llvm::StoreInst>(j)) {
-                        locals_end[bb][op->getName()] = store->getValueOperand();
-                        delete_instructions.insert(j);
-                    }
-                }
+    // insert loads at the start of BB
+    for (auto&& g : globals) {
+        auto&& gName = g.first();
+        auto&& gValue = g.second;
+        auto* load = new llvm::LoadInst(gValue, gName + LIFTED_POSTFIX, inst);
+        bb_locals_begin[gName] = bb_locals_end[gName] = load;
+    }
+
+    for ( ; inst != BB->end(); ++inst) {
+        // replace globals to locals in loads/stores
+        if (auto* load = llvm::dyn_cast<llvm::LoadInst>(inst)) {
+            if (auto* gv = llvm::dyn_cast<llvm::GlobalVariable>(load->getPointerOperand())) {
+                load->replaceAllUsesWith(bb_locals_end[gv->getName()]);
+                deleted_instructions.insert(load);
             }
-            //if j is terminate instruction - store all
-            if (auto* terminate = llvm::dyn_cast<llvm::TerminatorInst>(j)) {
-            	if(j->getOpcode() != llvm::Instruction::Br){
-					for (auto&& g : globals) {
-						auto&& gName = g.first();
-						new llvm::StoreInst(locals_end[bb][gName], globals[gName], terminate);
-					}
-               	}
-            //if j is call instruction - store all before call and load all after call
-            } else if (auto* call = llvm::dyn_cast<llvm::CallInst>(j)) {
-             	for (auto&& g : globals) {
-                    auto&& gName = g.first();
-              		new llvm::StoreInst(locals_end[bb][gName], globals[gName], call);
-               	}
-               	++j;
-               	for (auto&& g : globals) {
+        } else if (auto* store = llvm::dyn_cast<llvm::StoreInst>(inst)) {
+            if (auto* gv = llvm::dyn_cast<llvm::GlobalVariable>(store->getPointerOperand())) {
+                bb_locals_end[gv->getName()] = store->getValueOperand();
+                deleted_instructions.insert(store);
+            }
+        }
+        // store everything before terminators
+        else if (auto* terminator = llvm::dyn_cast<llvm::TerminatorInst>(inst)) {
+            if (llvm::Instruction::Br != terminator->getOpcode()) {
+                for (auto&& g : globals) {
                     auto&& gName = g.first();
                     auto&& gValue = g.second;
-                    auto* load = new llvm::LoadInst(gValue, gName + LOCAL_POSTFIX, j);
-                    locals_end[bb][gName] = load;
-              	}
-               	--j;
+                    new llvm::StoreInst(bb_locals_end[gName], gValue, terminator);
+                }
             }
+        // do store/load around calls to other functions
+        } else if (auto* call = llvm::dyn_cast<llvm::CallInst>(inst)) {
+            if (call->getCalledFunction()->doesNotAccessMemory()) continue;
+
+            auto&& next_inst = std::next(inst);
+            for (auto&& g : globals) {
+                auto&& gName = g.first();
+                auto&& gValue = g.second;
+                new llvm::StoreInst(bb_locals_end[gName], gValue, inst);
+                bb_locals_end[gName] = new llvm::LoadInst(gValue, gName + LIFTED_POSTFIX, next_inst);
+            }
+            inst = --next_inst;
         }
     }
+}
 
-    for (auto&& it = ++F.begin(); it != F.end(); ++it) {
-        auto* bb = &*it;
+void GlobalVariableLifting::linkBasicBlock(llvm::BasicBlock* BB) {
+    auto&& bb_locals_begin = locals_begin[BB];
+    auto&& bb_locals_end = locals_end[BB];
 
-        // add phi-nodes in blocks with several predecessors
-        if (bb->getUniquePredecessor() == nullptr) {
-            auto&& pred_num = 0U;
-            for (auto&& p = pred_begin(bb); p != pred_end(bb); ++p) ++pred_num;
-
-            for (auto&& g : globals) {
-                auto&& gName = g.first();
-                auto* phi = llvm::PHINode::Create(
-                    locals_begin[bb][gName]->getType(),
-                    pred_num,
-                    gName + LOCAL_POSTFIX,
-                    &bb->front()
-                );
-                for (auto&& p = pred_begin(bb); p != pred_end(bb); ++p) {
-                    auto* pred = *p;
-                    phi->addIncoming(locals_end[pred][gName], pred);
-                }
-                if (locals_begin[bb][gName] == locals_end[bb][gName]) {
-                    locals_end[bb][gName] = phi;
-                }
-                locals_begin[bb][gName]->replaceAllUsesWith(phi);
-                delete_instructions.insert(locals_begin[bb][gName]);
-                locals_begin[bb][gName] = phi;
+    // remove unused locals in BB with one predecessor
+    if (auto* pred = BB->getUniquePredecessor()) {
+        for (auto&& g : globals) {
+            auto&& gName = g.first();
+            auto&& pred_value = locals_end[pred][gName];
+            if (bb_locals_begin[gName] == bb_locals_end[gName]) {
+                bb_locals_end[gName] = pred_value;
             }
-        // remove unused locals in basic blocks with one predecessor
-        } else {
-            auto* pred = bb->getUniquePredecessor();
-            for (auto&& g : globals) {
-                auto&& gName = g.first();
-                if (locals_begin[bb][gName] == locals_end[bb][gName]) {
-                    locals_end[bb][gName] = locals_end[pred][gName];
-                }
-                locals_begin[bb][gName]->replaceAllUsesWith(locals_end[pred][gName]);
-                delete_instructions.insert(locals_begin[bb][gName]);
-                locals_begin[bb][gName] = locals_end[pred][gName];
-            }
+            bb_locals_begin[gName]->replaceAllUsesWith(pred_value);
+            deleted_instructions.insert(bb_locals_begin[gName]);
+            bb_locals_begin[gName] = pred_value;
         }
+    // add phi nodes in BB with several predecessors
+    } else {
+        auto&& pred_num = std::distance(llvm::pred_begin(BB), llvm::pred_end(BB));
+        for (auto&& g : globals) {
+            auto&& gName = g.first();
+            auto* phi = llvm::PHINode::Create(
+                    bb_locals_end[gName]->getType(),
+                    pred_num,
+                    gName + LIFTED_POSTFIX,
+                    &BB->front()
+            );
+            for (auto&& pred = llvm::pred_begin(BB); pred != llvm::pred_end(BB); ++pred) {
+                phi->addIncoming(locals_end[*pred][gName], *pred);
+            }
+            if (bb_locals_begin[gName] == bb_locals_end[gName]) {
+                bb_locals_end[gName] = phi;
+            }
+            bb_locals_begin[gName]->replaceAllUsesWith(phi);
+            deleted_instructions.insert(bb_locals_begin[gName]);
+            bb_locals_begin[gName] = phi;
+        }
+    }
+}
+
+bool GlobalVariableLifting::runOnFunction(llvm::Function& F) {
+    using namespace llvm;
+
+    init();
+
+    collectGlobals(F);
+
+    for (auto&& bb = F.begin(); bb != F.end(); ++bb) {
+        setupBasicBlock(bb);
+    }
+
+    for (auto&& bb = ++F.begin(); bb != F.end(); ++bb) {
+        linkBasicBlock(bb);
     }
 
     // deleting unused instructions
-    for (auto&& i : delete_instructions) {
-        if (auto* ii = llvm::dyn_cast<llvm::Instruction>(i)) {
-            ii->eraseFromParent();
-        }
+    for (auto&& i : util::viewContainer(deleted_instructions)
+                    .map(llvm::dyn_caster<llvm::Instruction>())
+                    .filter()) {
+        i->eraseFromParent();
     }
+
     return true;
 }
 
-char GlobalVariableLifting::ID=0;
-static llvm::RegisterPass<GlobalVariableLifting> X("gvl", "global variable lifting pass", false, false);
+char GlobalVariableLifting::ID = 0;
+
+static llvm::RegisterPass<GlobalVariableLifting>
+X("gvl", "global variable lifting pass", false, false);
 
 }  /* namespace borealis */
-
-
-
-
-
-
