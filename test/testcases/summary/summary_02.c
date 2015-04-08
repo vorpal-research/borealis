4c62fcabe4117e8139746f4d9d2dfea0baf6f0e0
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2015-04-08 16:00:13 +0300

    Tests are ok now

--- test/testcases/summary/summary_02.c
+++ test/testcases/summary/summary_02.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* my_malloc(size_t size, int safe) {
+// @requires \is_valid_ptr(arr)
+void another_malloc(int** arr, size_t size, int safe) {
     int* res = malloc(sizeof(int) * size);
     if (safe > 0) {
-        if (res) return res;
-        else exit(255);
+        if (res)
+            *arr = res;
+        else
+            exit(255);
     } else {
-        return res;
+        *arr = res;
     }
 }
 
@@ -18,7 +21,8 @@ void my_free(int* arr) {
 }
 
 int main() {
-    int* arr = my_malloc(16, 1);
+    int* arr;
+    another_malloc(&arr, 16, 1);
 
     for (int i = 0; i < 16; ++i) {
         arr[i] = i;
