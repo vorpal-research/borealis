b04eafab86de750a82e34b265a63c0854247f9f5
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2015-04-10 13:49:49 +0300

    AnnotationMaterializer adapted to new debug info policy for functions + force more unsignedness when needed

--- State/Transformer/AnnotationMaterializer.cpp
+++ State/Transformer/AnnotationMaterializer.cpp
@@ -6,11 +6,14 @@
  */
 
 #include "State/Transformer/AnnotationMaterializer.h"
+#include "Term/TermBuilder.h"
+#include "Util/util.h"
+
+#include "Util/macros.h"
 
 namespace borealis {
 
-class AnnotationMaterializer::AnnotationMaterializerImpl {
-public:
+struct AnnotationMaterializer::AnnotationMaterializerImpl {
     const LogicAnnotation* A;
     TermFactory::Ptr TF;
     VariableInfoTracker* MI;
@@ -37,17 +40,17 @@ AnnotationMaterializer::AnnotationMaterializer(
         llvm::isa<RequiresAnnotation>(A) ||
         llvm::isa<AssignsAnnotation>(A)) {
         pimpl->nc.placement = NameContext::Placement::OuterScope;
-        if (auto f = llvm::dyn_cast_or_null<llvm::Function>(
+        if (auto* f = llvm::dyn_cast_or_null<llvm::Function>(
                 pimpl->MI->locate(pimpl->nc.loc, DiscoveryPolicy::NextFunction).val
-            )) {
+        )) {
             pimpl->nc.func = f;
         }
     } else if (llvm::isa<AssertAnnotation>(A) ||
                llvm::isa<AssumeAnnotation>(A)) {
         pimpl->nc.placement = NameContext::Placement::InnerScope;
-        if (auto f = llvm::dyn_cast_or_null<llvm::Function>(
+        if (auto* f = llvm::dyn_cast_or_null<llvm::Function>(
                 pimpl->MI->locate(pimpl->nc.loc, DiscoveryPolicy::PreviousFunction).val
-            )) {
+        )) {
             pimpl->nc.func = f;
         }
     }
@@ -56,7 +59,7 @@ AnnotationMaterializer::AnnotationMaterializer(
 AnnotationMaterializer::~AnnotationMaterializer() {}
 
 Annotation::Ptr AnnotationMaterializer::doit() {
-    auto trm = transform(pimpl->A->getTerm());
+    auto&& trm = transform(pimpl->A->getTerm());
     return pimpl->A->clone(trm);
 }
 
@@ -65,7 +68,7 @@ llvm::LLVMContext& AnnotationMaterializer::getLLVMContext() const {
 }
 
 VariableInfoTracker::ValueDescriptor AnnotationMaterializer::forName(const std::string& name) const {
-    switch(pimpl->nc.placement) {
+    switch (pimpl->nc.placement) {
     case NameContext::Placement::GlobalScope:
     case NameContext::Placement::InnerScope:
         return pimpl->MI->locate(name, pimpl->A->getLocus(), DiscoveryPolicy::PreviousVal);
@@ -86,6 +89,12 @@ VariableInfoTracker::ValueDescriptors AnnotationMaterializer::forValue(llvm::Val
     return pimpl->MI->locate(value);
 }
 
+VariableInfoTracker::ValueDescriptor AnnotationMaterializer::forValueSingle(llvm::Value* value) const {
+    auto&& descs = forValue(value);
+    ASSERTC(descs.size() == 1);
+    return descs.front();
+}
+
 void AnnotationMaterializer::failWith(const std::string& message) {
     static std::string buf;
 
@@ -99,6 +108,255 @@ void AnnotationMaterializer::failWith(const std::string& message) {
 
     buf = str.str();
     throw std::runtime_error(buf.c_str());
+}
+
+Term::Ptr AnnotationMaterializer::transformOpaqueCall(OpaqueCallTermPtr trm) {
+    if (auto* builtin = llvm::dyn_cast<OpaqueBuiltinTerm>(trm->getLhv())) {
+        if (builtin->getVName() == "property") {
+            auto&& rhv = trm->getRhv().toVector();
+            if (rhv.size() != 2) failWith("Illegal \\property access " + trm->getName() + ": exactly two operands expected");
+
+            auto&& prop = FN.Term->getOpaqueConstantTerm(rhv[0]->getName());
+            auto&& val = this->transform(rhv[1]);
+
+            return FN.Term->getReadPropertyTerm(FN.Type->getUnknownType(), prop, val);
+
+        } else if (builtin->getVName() == "bound") {
+            auto&& rhv = trm->getRhv().toVector();
+            if (rhv.size() != 1) failWith("Illegal \\bound access " + trm->getName() + ": exactly one operand expected");
+
+            auto&& val = this->transform(rhv[0]);
+
+            return FN.Term->getBoundTerm(val);
+
+        } else if (builtin->getVName() == "is_valid_ptr") {
+            auto&& rhv = trm->getRhv().toVector();
+            if (rhv.size() != 1) failWith("Illegal \\is_valid_ptr access " + trm->getName() + ": exactly one operand expected");
+
+            auto&& val = this->transform(rhv[0]);
+            auto&& type = val->getType();
+
+            if (auto* ptrType = llvm::dyn_cast<type::Pointer>(type)) {
+                auto&& pointed = ptrType->getPointed();
+
+                auto&& bval = builder(val);
+
+                return bval != null()
+                       && bval != invalid()
+                       && bval.bound().uge( builder(TypeUtils::getTypeSizeInElems(pointed)) );
+            } else {
+                failWith("Illegal \\is_valid_ptr access " + trm->getName() + ": called on non-pointer");
+            }
+        } else if (builtin->getVName() == "old") {
+            // all \old's should be already taken care of, let's try to guess the problem
+            if (trm->getRhv().size() != 1) failWith("Illegal \\old invocation " + trm->getName() + ": exactly one operand expected");
+            else failWith("Malformed \\old invocation");
+        } else {
+            failWith("Cannot call " + trm->getName() + ": not supported");
+        }
+    } else {
+        failWith("Cannot call " + trm->getName() + ": only builtins can be called in this way");
+    }
+
+    BYE_BYE(Term::Ptr, "Unreachable!");
+}
+
+Term::Ptr AnnotationMaterializer::transformDirectOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm) {
+    auto&& arg = trm->getLhv();
+
+    auto&& load = llvm::dyn_cast<LoadTerm>(arg);
+    if (not load)
+        failWith(
+            "Cannot access member " +
+            trm->getProperty() +
+            ": term is not an instance of load"
+        );
+
+    auto&& daStruct = llvm::dyn_cast<type::Record>(load->getType());
+    if (not daStruct)
+        failWith(
+            "Cannot access member " +
+            trm->getProperty() +
+            ": term is not a structure type"
+        );
+
+    auto&& field = daStruct->getBody()->get().getFieldByName(trm->getProperty());
+    if (not field)
+        failWith(
+            "Cannot access member " +
+            trm->getProperty() +
+            ": no such member defined or structure not available on " +
+            util::toString(*daStruct)
+        );
+
+    return *builder(load->getRhv()).gep(builder(0), builder(field.getUnsafe().getIndex()));
+}
+
+Term::Ptr AnnotationMaterializer::transformIndirectOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm) {
+    auto&& arg = trm->getLhv();
+
+    auto&& daPointer = llvm::dyn_cast<type::Pointer>(arg->getType());
+    if (not daPointer)
+        failWith(
+            "Cannot access member " +
+            trm->getProperty() +
+            ": term is not a pointer"
+        );
+
+    auto&& daStruct = llvm::dyn_cast<type::Record>(daPointer->getPointed());
+    if (not daStruct)
+        failWith(
+            "Cannot access member " +
+            trm->getProperty() +
+            ": term is not a pointer to a structure type"
+        );
+
+    auto&& field = daStruct->getBody()->get().getFieldByName(trm->getProperty());
+    if (not field)
+        failWith(
+            "Cannot access member " +
+            trm->getProperty() +
+            ": no such member defined or structure not available on " +
+            util::toString(*daStruct)
+        );
+
+    return *builder(arg).gep(builder(0), builder(field.getUnsafe().getIndex()));
+}
+
+Term::Ptr AnnotationMaterializer::transformOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm) {
+    return trm->isIndirect() ?
+           transformIndirectOpaqueMemberAccessTerm(trm) :
+           transformDirectOpaqueMemberAccessTerm(trm);
+}
+
+Term::Ptr AnnotationMaterializer::transformArrayOpaqueIndexingTerm(OpaqueIndexingTermPtr trm) {
+    auto&& arg = trm->getLhv();
+
+    auto&& load = llvm::dyn_cast<LoadTerm>(arg);
+    if (not load) failWith("Cannot access member: term is not an instance of load");
+
+    auto&& daArray = llvm::dyn_cast<type::Array>(load->getType());
+    if (not daArray) failWith("Cannot access element: term is not an array type");
+
+    return *builder(load->getRhv()).gep(builder(0), trm->getRhv());
+}
+
+Term::Ptr AnnotationMaterializer::transformOpaqueIndexingTerm(OpaqueIndexingTermPtr trm) {
+    // FIXME: handle multidimensional arrays
+    if (llvm::isa<type::Array>(trm->getType())) {
+        return transformArrayOpaqueIndexingTerm(trm);
+    }
+
+    auto&& gep = builder(trm->getLhv()).gep(trm->getRhv())();
+
+    // check types
+    if (auto* terr = llvm::dyn_cast<type::TypeError>(gep->getType())) {
+        failWith(terr->getMessage());
+    } else if (llvm::isa<type::Pointer>(gep->getType())) {
+        return *builder(gep);
+    } else {
+        failWith("Type " + util::toString(gep->getType()) + " not dereferenceable");
+    }
+
+    BYE_BYE(Term::Ptr, "Unreachable!");
+}
+
+Term::Ptr AnnotationMaterializer::transformOpaqueVarTerm(OpaqueVarTermPtr trm) {
+    auto&& ret = forName(trm->getVName());
+    if (ret.isInvalid()) failWith(trm->getVName() + " : variable not found in scope");
+
+//    FIXME: akhin WTF???
+//    auto&& bcType = ret.val->getType();
+//    if (ret.shouldBeDereferenced) {
+//        if (not bcType->isPointerTy()) failWith("wtf");
+//        bcType = bcType->getPointerElementType();
+//    }
+
+    auto&& shouldBeDereferenced = ret.shouldBeDereferenced;
+    // FIXME: Need to sort out memory model
+    //        Global arrays and structures break things...
+    if (auto* ptrType = llvm::dyn_cast<llvm::PointerType>(ret.val->getType())) {
+        shouldBeDereferenced &= ptrType->getPointerElementType()->isSingleValueType();
+    }
+
+    auto&& var = factory().getValueTerm(ret.val, ret.type.getSignedness());
+
+    if (shouldBeDereferenced) {
+        return *builder(var);
+    } else {
+        return var;
+    }
+}
+
+Term::Ptr AnnotationMaterializer::transformOpaqueBuiltinTerm(OpaqueBuiltinTermPtr trm) {
+    const llvm::StringRef name{ trm->getVName() };
+    auto&& ctx = nameContext();
+
+    if (name == "result") {
+        if (ctx.func && ctx.placement == NameContext::Placement::OuterScope) {
+            auto&& desc = forValueSingle(ctx.func);
+            auto&& funcType = DICompositeType(desc.type);
+            auto&& retType = funcType.getTypeArray().getElement(0);
+            auto&& retType_ = stripAliases(pimpl->MI->getDebugInfo(), retType);
+            return factory().getReturnValueTerm(ctx.func, retType_.getSignedness());
+
+        } else {
+            failWith("\result can only be bound to functions' outer scope");
+        }
+
+    } else if (name == "null" || name == "nullptr") {
+        return null();
+
+    } else if (name == "invalid" || name == "invalidptr") {
+        return invalid();
+
+    } else if (name.startswith("arg")) {
+        if (ctx.func && ctx.placement == NameContext::Placement::OuterScope) {
+            std::istringstream ist(name.drop_front(3).str());
+            unsigned val = 0U;
+            ist >> val;
+
+            if (ctx.func->isDeclaration()) failWith("annotations on function declarations are not supported");
+
+            ASSERTC(val < ctx.func->arg_size());
+
+            auto&& arg = ctx.func->arg_begin();
+            std::advance(arg, val);
+
+            auto&& desc = forValueSingle(ctx.func);
+            return factory().getArgumentTerm(arg, desc.type.getSignedness());
+
+        } else {
+            failWith("\\argXXX can only be bound to functions' outer scope");
+        }
+    } else {
+        failWith("\\" + name + " : unknown builtin");
+    }
+
+    BYE_BYE(Term::Ptr, "Unreachable!");
+};
+
+Term::Ptr AnnotationMaterializer::transformCmpTerm(CmpTermPtr trm) {
+    auto&& lhvt = trm->getLhv()->getType();
+    auto&& rhvt = trm->getRhv()->getType();
+
+    // XXX: Tricky stuff follows...
+    //      CmpTerm from annotations is signed by default,
+    //      need to change that to unsigned when needed
+    if (auto match = util::match_pair<type::Integer, type::Integer>(lhvt, rhvt)) {
+        if (
+            match->first->getSignedness() == llvm::Signedness::Unsigned ||
+            match->second->getSignedness() == llvm::Signedness::Unsigned
+        ) {
+            return factory().getCmpTerm(
+                llvm::forceUnsigned(trm->getOpcode()),
+                trm->getLhv(),
+                trm->getRhv()
+            );
+        }
+    }
+
+    return trm;
 }
 
 Annotation::Ptr materialize(
@@ -115,3 +373,5 @@ Annotation::Ptr materialize(
 }
 
 } // namespace borealis
+
+#include "Util/unmacros.h"
--- State/Transformer/AnnotationMaterializer.h
+++ State/Transformer/AnnotationMaterializer.h
@@ -15,18 +15,14 @@
 #include "Passes/Tracker/VariableInfoTracker.h"
 #include "State/Transformer/Transformer.hpp"
 #include "Term/NameContext.h"
-#include "Term/TermBuilder.h"
-#include "Util/util.h"
-
-#include "Util/macros.h"
 
 namespace borealis {
 
 class AnnotationMaterializer : public borealis::Transformer<AnnotationMaterializer> {
 
-    typedef borealis::Transformer<AnnotationMaterializer> Base;
+    using Base = borealis::Transformer<AnnotationMaterializer>;
 
-    class AnnotationMaterializerImpl;
+    struct AnnotationMaterializerImpl;
     std::unique_ptr<AnnotationMaterializerImpl> pimpl;
 
 public:
@@ -43,272 +39,33 @@ public:
     TermFactory& factory() const;
 
     VariableInfoTracker::ValueDescriptors forValue(llvm::Value* value) const;
-    VariableInfoTracker::ValueDescriptor forValueSingle(llvm::Value* value) const {
-        auto descs = forValue(value);
-        ASSERTC(descs.size() == 1);
-        return descs.front();
-    }
+    VariableInfoTracker::ValueDescriptor forValueSingle(llvm::Value* value) const;
 
     Annotation::Ptr doit();
 
     void failWith(const std::string& message);
-    inline void failWith(llvm::Twine twine) {
-        failWith(twine.str());
-    }
+    inline void failWith(llvm::Twine twine) { failWith(twine.str()); }
     // resolving ambiguity
-    inline void failWith(const char* r) {
-        failWith(std::string(r));
-    }
+    inline void failWith(const char* r) { failWith(std::string(r)); }
 
     // note this is called without a "Term" at the end, meaning
     // it is called before (and instead of) transforming children
-    Term::Ptr transformOpaqueCall(OpaqueCallTermPtr trm) {
-        if(auto builtin = llvm::dyn_cast<OpaqueBuiltinTerm>(trm->getLhv())) {
-            if(builtin->getVName() == "property") {
-                auto rhv = trm->getRhv().toVector();
-                if(rhv.size() != 2) failWith("Illegal \\property access " + trm->getName() + ": exactly two operands expected");
+    Term::Ptr transformOpaqueCall(OpaqueCallTermPtr trm);
 
-                auto prop = FN.Term->getOpaqueConstantTerm(rhv[0]->getName());
-                auto val = this->transform(rhv[1]);
+    Term::Ptr transformDirectOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm);
+    Term::Ptr transformIndirectOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm);
+    Term::Ptr transformOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm);
+    Term::Ptr transformArrayOpaqueIndexingTerm(OpaqueIndexingTermPtr trm);
+    Term::Ptr transformOpaqueIndexingTerm(OpaqueIndexingTermPtr trm);
+    Term::Ptr transformOpaqueVarTerm(OpaqueVarTermPtr trm);
+    Term::Ptr transformOpaqueBuiltinTerm(OpaqueBuiltinTermPtr trm);
 
-                return FN.Term->getReadPropertyTerm(FN.Type->getUnknownType(), prop, val);
+    Term::Ptr transformCmpTerm(CmpTermPtr trm);
 
-            } else if(builtin->getVName() == "bound") {
-                auto rhv = trm->getRhv().toVector();
-                if(rhv.size() != 1) failWith("Illegal \\bound access " + trm->getName() + ": exactly one operand expected");
-
-                auto val = this->transform(rhv[0]);
-
-                return FN.Term->getBoundTerm(val);
-
-            } else if(builtin->getVName() == "is_valid_ptr") {
-                auto rhv = trm->getRhv().toVector();
-                if(rhv.size() != 1) failWith("Illegal \\is_valid_ptr access " + trm->getName() + ": exactly one operand expected");
-
-                auto val = this->transform(rhv[0]);
-                auto type = val->getType();
-
-                if (auto ptrType = llvm::dyn_cast<type::Pointer>(type)) {
-                    auto pointed = ptrType->getPointed();
-
-                    auto bval = builder(val);
-
-                    return bval != null()
-                        && bval != invalid()
-                        && bval.bound().uge (builder(TypeUtils::getTypeSizeInElems(pointed)));
-                  } else failWith("Illegal \\is_valid_ptr access " + trm->getName() + ": called on non-pointer");
-            } else if(builtin->getVName() == "old") {
-                // all \old's should be already taken care of, let's try to guess the problem
-                if(trm->getRhv().size() != 1)
-                    failWith("Illegal \\old invocation " + trm->getName() + ": exactly one operand expected");
-                failWith("Malformed \\old invocation");
-            } else failWith("Cannot call " + trm->getName() + ": not supported");
-        } else failWith("Cannot call " + trm->getName() + ": only builtins can be called in this way");
-
-        BYE_BYE(Term::Ptr, "Unreachable!");
-    }
-
-    Term::Ptr transformDirectOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm) {
-        auto arg = trm->getLhv();
-
-        auto load = llvm::dyn_cast<LoadTerm>(arg);
-        if(!load) failWith(
-            "Cannot access member " +
-            trm->getProperty() +
-            ": term is not an instance of load"
-        );
-
-        auto daStruct = llvm::dyn_cast<type::Record>(load->getType());
-        if(!daStruct) failWith(
-            "Cannot access member " +
-            trm->getProperty() +
-            ": term is not a structure type"
-        );
-
-        auto field = daStruct->getBody()->get().getFieldByName(trm->getProperty());
-        if(!field) failWith(
-            "Cannot access member " +
-            trm->getProperty() +
-            ": no such member defined or structure not available on " +
-            util::toString(*daStruct)
-        );
-
-        return *builder(load->getRhv()).gep(builder(0), builder(field.getUnsafe().getIndex()));
-    }
-
-    Term::Ptr transformIndirectOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm) {
-        auto arg = trm->getLhv();
-
-        auto daPointer = llvm::dyn_cast<type::Pointer>(arg->getType());
-        if(!daPointer) failWith(
-            "Cannot access member " +
-            trm->getProperty() +
-            ": term is not a pointer"
-        );
-
-        auto daStruct = llvm::dyn_cast<type::Record>(daPointer->getPointed());
-        if(!daStruct) failWith(
-            "Cannot access member " +
-            trm->getProperty() +
-            ": term is not a pointer to a structure type"
-        );
-
-
-        auto field = daStruct->getBody()->get().getFieldByName(trm->getProperty());
-        if(!field) {
-            failWith(
-                "Cannot access member " +
-                trm->getProperty() +
-                ": no such member defined or structure not available on " +
-                util::toString(*daStruct)
-            );
-        }
-
-        return *builder(arg).gep(builder(0), builder(field.getUnsafe().getIndex()));
-    }
-
-    Term::Ptr transformOpaqueMemberAccessTerm(OpaqueMemberAccessTermPtr trm) {
-        return trm->isIndirect() ?
-               transformIndirectOpaqueMemberAccessTerm(trm) :
-               transformDirectOpaqueMemberAccessTerm(trm);
-    }
-
-    Term::Ptr transformArrayOpaqueIndexingTerm(OpaqueIndexingTermPtr trm) {
-        auto arg = trm->getLhv();
-
-        auto load = llvm::dyn_cast<LoadTerm>(arg);
-        if(!load) failWith("Cannot access member: term is not an instance of load");
-
-        auto daArray = llvm::dyn_cast<type::Array>(load->getType());
-        if(!daArray) failWith("Cannot access element: term is not an array type");
-
-        return *builder(load->getRhv()).gep(builder(0), trm->getRhv());
-
-    }
-
-    Term::Ptr transformOpaqueIndexingTerm(OpaqueIndexingTermPtr trm) {
-        // handle multidimensional arrays
-        if(llvm::isa<type::Array>(trm->getType())) {
-            return transformArrayOpaqueIndexingTerm(trm);
-        }
-
-        auto gep = builder(trm->getLhv()).gep(trm->getRhv())();
-
-        // check types
-        if(auto* terr = llvm::dyn_cast<type::TypeError>(gep->getType())) {
-            failWith(terr->getMessage());
-        } else if(llvm::isa<type::Pointer>(gep->getType())) {
-            return factory().getLoadTerm(gep);
-        } else failWith("Type " + util::toString(gep->getType()) + " not dereferenceable");
-
-        BYE_BYE(Term::Ptr, "Unreachable!");
-    }
-
-    Term::Ptr transformOpaqueVarTerm(OpaqueVarTermPtr trm) {
-        auto ret = forName(trm->getVName());
-        if (ret.isInvalid()) failWith(trm->getVName() + " : variable not found in scope");
-
-        auto bcType = ret.val->getType();
-        if (ret.shouldBeDereferenced) {
-            if(!bcType->isPointerTy()) failWith("wtf");
-            bcType = bcType->getPointerElementType();
-        }
-
-        auto shouldBeDereferenced = ret.shouldBeDereferenced;
-        // FIXME: Need to sort out memory model
-        //        Global arrays and structures break things...
-        if (auto* ptrType = llvm::dyn_cast<llvm::PointerType>(ret.val->getType())) {
-            shouldBeDereferenced &= ptrType->getPointerElementType()->isSingleValueType();
-        }
-
-        auto var = factory().getValueTerm(ret.val, ret.type.getSignedness());
-
-        if (shouldBeDereferenced) {
-            return factory().getLoadTerm(var);
-        } else {
-            return var;
-        }
-    }
-
-    Term::Ptr transformOpaqueBuiltinTerm(OpaqueBuiltinTermPtr trm) {
-        const llvm::StringRef name = trm->getVName();
-        const auto& ctx = nameContext();
-
-        if (name == "result") {
-            if (ctx.func && ctx.placement == NameContext::Placement::OuterScope) {
-                auto desc = forValueSingle(ctx.func);
-
-                return factory().getReturnValueTerm(ctx.func, desc.type.getSignedness());
-
-            } else failWith("\result can only be bound to functions' outer scope");
-
-        } else if (name == "null" || name == "nullptr") {
-            return factory().getNullPtrTerm();
-        } else if (name == "invalid" || name == "invalidptr") {
-            return factory().getInvalidPtrTerm();
-        } else if (name.startswith("arg")) {
-            if (ctx.func && ctx.placement == NameContext::Placement::OuterScope) {
-                std::istringstream ist(name.drop_front(3).str());
-                unsigned val = 0U;
-                ist >> val;
-
-                if(ctx.func->isDeclaration())
-                    failWith("annotations on function declarations are not supported");
-
-                ASSERTC(val < ctx.func->arg_size());
-
-                auto arg = ctx.func->arg_begin();
-                std::advance(arg, val);
-
-                auto desc = forValueSingle(ctx.func);
-
-                return factory().getArgumentTerm(arg, desc.type.getSignedness());
-
-            } else {
-                failWith("\argXXX can only be bound to functions' outer scope");
-            }
-        } else {
-            failWith("\\" + name + " : unknown builtin");
-        }
-
-        BYE_BYE(Term::Ptr, "Unreachable!");
-    }
-
-    Term::Ptr transformCmpTerm(CmpTermPtr trm) {
-        using borealis::util::match_pair;
-
-        auto lhvt = trm->getLhv()->getType();
-        auto rhvt = trm->getRhv()->getType();
-
-        // XXX: Tricky stuff follows...
-        //      CmpTerm from annotations is signed by default,
-        //      need to change that to unsigned when needed
-        if (auto match = match_pair<type::Integer, type::Integer>(lhvt, rhvt)) {
-            if (
-                    (
-                        match->first->getSignedness() == llvm::Signedness::Unsigned &&
-                        match->second->getSignedness() != llvm::Signedness::Signed
-                    ) || (
-                        match->second->getSignedness() == llvm::Signedness::Unsigned &&
-                        match->first->getSignedness() != llvm::Signedness::Signed
-                    )
-            ) {
-                return factory().getCmpTerm(
-                    llvm::forceUnsigned(trm->getOpcode()),
-                    trm->getLhv(),
-                    trm->getRhv()
-                );
-            }
-        }
-
-        return trm;
-    }
 };
 
 Annotation::Ptr materialize(Annotation::Ptr, FactoryNest FN, VariableInfoTracker*);
 
 } /* namespace borealis */
-
-#include "Util/unmacros.h"
 
 #endif /* ANNOTATIONMATERIALIZER_H_ */
