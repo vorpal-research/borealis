8c358f5859c4a80b2d4b6fa854a04dfbaa4d4596
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2015-04-28 18:46:33 +0300

    Even better Z3 stuff output

--- SMT/Z3/Z3.cpp
+++ SMT/Z3/Z3.cpp
@@ -7,23 +7,26 @@
 
 #include "Util/macros.h"
 
-namespace borealis {
-namespace z3_ {
+namespace z3 {
 
-std::ostream& operator<<(std::ostream& os, const z3::func_interp& z3fi) {
-    auto&& entries = util::range(0U, z3fi.num_entries()).map(APPLY(z3fi.entry));
+borealis::logging::logstream& operator<<(borealis::logging::logstream& os, const z3::func_interp& z3fi) {
+    auto&& entries = borealis::util::range(0U, z3fi.num_entries()).map(APPLY(z3fi.entry));
     for (auto&& e : entries) {
-        os << "  " << e << std::endl;
+        os << e << borealis::logging::endl;
     }
-    return os << "  " << z3fi.else_value();
+    return os << "else -> " << z3fi.else_value();
 }
 
-std::ostream& operator<<(std::ostream& os, const z3::func_entry& z3fe) {
-    auto&& args = util::range(0U, z3fe.num_args()).map(APPLY(z3fe.arg)).toVector();
+borealis::logging::logstream& operator<<(borealis::logging::logstream& os, const z3::func_entry& z3fe) {
+    auto&& args = borealis::util::range(0U, z3fe.num_args()).map(APPLY(z3fe.arg)).toVector();
     return os << args << " -> " << z3fe.value();
 }
 
-} // namespace z3_
-} // namespace borealis
+borealis::logging::logstream& operator<<(borealis::logging::logstream& os, const z3::expr_vector& z3ev) {
+    for (size_t i = 0U; i < z3ev.size(); ++i) os << z3ev[i] << borealis::logging::endl;
+    return os;
+}
+
+} // namespace z3
 
 #include "Util/unmacros.h"
--- SMT/Z3/Z3.h
+++ SMT/Z3/Z3.h
@@ -55,11 +55,14 @@ struct Z3 {
     
 };
 
-namespace z3_ {
-std::ostream& operator<<(std::ostream& os, const z3::func_interp& z3fi);
-std::ostream& operator<<(std::ostream& os, const z3::func_entry& z3fe);
-} // namespace z3_
-
 } // namespace borealis
+
+namespace z3 {
+
+borealis::logging::logstream& operator<<(borealis::logging::logstream& os, const z3::expr_vector& z3ev);
+borealis::logging::logstream& operator<<(borealis::logging::logstream& os, const z3::func_entry& z3fe);
+borealis::logging::logstream& operator<<(borealis::logging::logstream& os, const z3::func_interp& z3fi);
+
+} // namespace z3
 
 #endif /* BOREALIS_Z3_H_ */
