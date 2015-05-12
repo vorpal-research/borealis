c48b93ef277e03687604361e5424d65e5e9bafb3
Mikhail Belyaev <devnull@localhost>
2015-05-12 20:04:47 +0300

    m

---
+++ Codegen/CType/CStructMember.cpp
@@ -0,0 +1,5 @@
+//
+// Created by belyaev on 5/12/15.
+//
+
+#include "CStructMember.h"
---
+++ Codegen/CType/CStructMember.h
@@ -0,0 +1,41 @@
+//
+// Created by belyaev on 5/12/15.
+//
+
+#ifndef C_STRUCT_MEMBER_H
+#define C_STRUCT_MEMBER_H
+
+#include <string>
+#include "Codegen/CType/CTypeRef.h"
+
+namespace borealis {
+
+class CStructMember {
+public:
+    CStructMember(size_t offset, const std::string& name, const CTypeRef& type) :
+        offset(offset), name(name), type(type) { }
+    CStructMember(const CStructMember&) = default;
+
+private:
+    size_t offset;
+    std::string name;
+    CTypeRef type;
+
+public:
+
+    size_t getOffset() const {
+        return offset;
+    }
+
+    const std::string& getName() const {
+        return name;
+    }
+
+    const CTypeRef& getType() const {
+        return type;
+    }
+};
+
+} /* namespace borealis */
+
+#endif /* C_STRUCT_MEMBER_H */
