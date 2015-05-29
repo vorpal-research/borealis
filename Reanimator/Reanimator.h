ee10589f3866806a2548c08a08cee7889384f897
Marat Kh. Akhin <akhin@kspt.ftk.spbstu.ru>
2015-05-29 14:25:23 +0300

    Experimental ReanimatorPass + m

--- Reanimator/Reanimator.h
+++ Reanimator/Reanimator.h
@@ -7,39 +7,45 @@
 
 #include "SMT/Result.h"
 #include "Term/Term.h"
+#include "State/Transformer/ArrayBoundsCollector.h"
 
 namespace borealis {
 
 class Reanimator {
 
-    friend class View;
-    friend class RaisingTypeVisitor;
-
 public:
 
-    class View {
-    public:
-        View(const Reanimator& r, Term::Ptr term);
-        friend borealis::logging::logstream& operator<<(
-            borealis::logging::logstream& s,
-            const Reanimator::View& rv
-        );
-    private:
-        const Reanimator& r;
-        Term::Ptr term;
-    };
+    using ArrayBounds = std::unordered_set<unsigned long long>;
+    using ArrayBoundsMap = std::unordered_map<unsigned long long, ArrayBounds>;
 
-    Reanimator(const smt::SatResult& result);
+    Reanimator(const smt::SatResult& result, const ArrayBoundsCollector::ArrayBounds& arrayBounds);
 
     const smt::SatResult& getResult() const;
-
-    View raise(Term::Ptr value) const;
+    const ArrayBoundsMap& getArrayBoundsMap() const;
+    const ArrayBounds& getArrayBounds(unsigned long long base) const;
 
 private:
 
-    const smt::SatResult& result;
+    smt::SatResult result;
+    ArrayBoundsMap arrayBoundsMap;
+
+    void processArrayBounds(const ArrayBoundsCollector::ArrayBounds& arrayBounds);
 
 };
+
+class ReanimatorView {
+public:
+    ReanimatorView(const Reanimator& r, Term::Ptr term);
+    friend borealis::logging::logstream& operator<<(
+        borealis::logging::logstream& s,
+        const ReanimatorView& rv
+    );
+private:
+    Reanimator r;
+    Term::Ptr term;
+};
+
+ReanimatorView raise(const Reanimator& r, Term::Ptr value);
 
 } // namespace borealis
 
