7e41a433b892627ba7d6377d370904db085dfa9d
Mikhail Belyaev <devnull@localhost>
2016-12-14 20:31:42 +0300

    m

--- Util/llvm_matchers.hpp
+++ Util/llvm_matchers.hpp
@@ -10,98 +10,87 @@
 #include <llvm/IR/Instructions.h>
 #include <llvm/ADT/ArrayRef.h>
 
+#include "Util/macros.h"
+
 namespace llvm {
 
 using namespace functional_hell::matchers;
 
 template<class Wha>
 static auto pattern_matcher(Wha pattern) {
-    return [&pattern](llvm::Value* v) { return !!(pattern >> v); };
+    return [&pattern](llvm::Value* v) { return static_cast<bool>(pattern >> v); };
 }
 
-static auto $OfType = functional_hell::matchers::function_as_pattern([](auto&& v){ return make_storage(v->getType()); });
+static auto $OfType = function_as_pattern([](auto&& v){ return make_storage(v->getType()); });
 
 template <class Derived, class F>
-static auto fwdAsDynCast(llvm::Value* v, F f) -> decltype(f(cast<Derived>(v))) {
+inline auto fwdAsDynCast(llvm::Value* v, F f) -> decltype(f(cast<Derived>(v))) {
     if(auto der = dyn_cast<Derived>(v)) return f(der);
-    else return functional_hell::matchers::no_match;
+    else return no_match;
 }
 
 struct ExtractValueExtractor {
-
-    storage_t<Value*, ArrayRef<unsigned>> unapply(Value* v) const {
-        if (auto&& tt = dyn_cast<ExtractValueInst>(v)) {
-            return make_storage(tt->getAggregateOperand(), tt->getIndices());
-        } else {
-            return {};
-        }
+    auto unapply(Value* v) const {
+        return fwdAsDynCast<ExtractValueInst>(v, LAM(tt, make_storage(tt->getAggregateOperand(), tt->getIndices())));
     }
-
 };
 
-static auto $ExtractValueInst = functional_hell::matchers::make_pattern(ExtractValueExtractor{});
+static auto $ExtractValueInst = make_pattern(ExtractValueExtractor{});
 
 struct InsertValueExtractor {
-
-    storage_t<Value*, Value*, ArrayRef<unsigned>> unapply(Value* v) const {
-        if (auto&& tt = dyn_cast<InsertValueInst>(v)) {
-            return functional_hell::matchers::make_storage(tt->getAggregateOperand(), tt->getInsertedValueOperand(), tt->getIndices());
-        } else {
-            return {};
-        }
+    auto unapply(Value* v) const {
+        return fwdAsDynCast<InsertValueInst>(
+            v,
+            LAM(tt, make_storage(tt->getAggregateOperand(), tt->getInsertedValueOperand(), tt->getIndices()))
+        );
     }
-
 };
 
-static auto $InsertValueInst = functional_hell::matchers::make_pattern(InsertValueExtractor{});
+static auto $InsertValueInst = make_pattern(InsertValueExtractor{});
 
 struct LoadExtractor {
-
-    storage_t<Value*> unapply(Value* v) const {
-        if(auto&& tt = dyn_cast<LoadInst>(v)) {
-            return make_storage(tt->getPointerOperand());
-        } else {
-            return {};
-        }
+    auto unapply(Value* v) const {
+        return fwdAsDynCast<LoadInst>(
+            v,
+            LAM(tt, make_storage(tt->getPointerOperand()))
+        );
     }
-
 };
 
-static auto $LoadInst = functional_hell::matchers::make_pattern(LoadExtractor{});
+static auto $LoadInst = make_pattern(LoadExtractor{});
 
 struct StoreExtractor {
-
-    storage_t<Value*, Value*> unapply(Value* v) const {
-        if(auto&& tt = dyn_cast<StoreInst>(v)) {
-            return make_storage(tt->getPointerOperand(), tt->getValueOperand());
-        } else return {};
+    auto unapply(Value* v) const {
+        return fwdAsDynCast<StoreInst>(
+            v,
+            LAM(tt, make_storage(tt->getPointerOperand(), tt->getValueOperand()))
+        );
     }
-
 };
 
-static auto $StoreInst = functional_hell::matchers::make_pattern(StoreExtractor{});
+static auto $StoreInst = make_pattern(StoreExtractor{});
 
 struct CallExtractor {
     auto unapply(Value* v) const {
-        return fwdAsDynCast<CallInst>(v, [](auto&& cc) { return make_storage(cc->getCalledValue(), cc->arg_operands()); }  );
+        return fwdAsDynCast<CallInst>(v, LAM(cc, make_storage(cc->getCalledValue(), cc->arg_operands())));
     }
-
 };
 
-static auto $CallInst = functional_hell::matchers::make_pattern(CallExtractor{});
+static auto $CallInst = make_pattern(CallExtractor{});
 
 struct IntrinsicExtractor {
     storage_t<Intrinsic::ID> unapply(Value* v) const {
-        if(auto&& f = dyn_cast<Function>(v)) {
-            if(f->isIntrinsic()) {
-                return make_storage((Intrinsic::ID)f->getIntrinsicID());
-            } else return {};
-        } else return {};
+        return fwdAsDynCast<Function>(
+            v,
+            LAM(f, f->isIntrinsic()? make_storage(Intrinsic::ID(f->getIntrinsicID())) : no_match)
+        );
     }
 };
 
-static auto $Intrinsic = functional_hell::matchers::make_pattern(IntrinsicExtractor{});
+static auto $Intrinsic = make_pattern(IntrinsicExtractor{});
 
 } /* namespace llvm */
+
+#include "Util/unmacros.h"
 
 #endif //LLVM_MATCHERS_HPP
