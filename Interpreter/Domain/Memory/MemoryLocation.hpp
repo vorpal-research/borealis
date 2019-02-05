ee26e77374439f2418906b22e3ed5e028f1c006b
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2019-02-05 14:06:33 +0300

    ptr

--- Interpreter/Domain/Memory/MemoryLocation.hpp
+++ Interpreter/Domain/Memory/MemoryLocation.hpp
@@ -12,11 +12,21 @@ namespace borealis {
 namespace absint {
 
 template <typename MachineInt>
+class NullLocation;
+
+template <typename MachineInt, typename Element>
+class ArrayLocation;
+
+template <typename MachineInt, typename ...Element>
+class StructLocation;
+
+template <typename MachineInt>
 class MemoryLocation : public AbstractDomain<MemoryLocation<MachineInt>> {
 public:
     enum Kind {
         ARRAY,
-        STRUCT
+        STRUCT,
+        NULL_
     };
 
 private:
@@ -35,16 +45,134 @@ public:
 
     MemoryLocation() : kind_(ARRAY) {}
     explicit MemoryLocation(Kind kind) : kind_(kind) {}
+    ~MemoryLocation() override = default;
+
+    template <typename MInt>
+    static std::shared_ptr<MemoryLocation<MInt>> makeNull() {
+        return NullLocation<MInt>::instance();
+    }
+
+    template <typename MInt, typename Element>
+    static std::shared_ptr<MemoryLocation<MInt>> makeArray(
+            typename ArrayLocation<MInt, Element>::LocPtr ptr,
+            typename ArrayLocation<MInt, Element>::IntervalPtr offset) {
+        return std::make_shared<ArrayLocation<MInt, Element>>(ptr, offset);
+    }
+
+    template <typename MInt, typename ...Element>
+    static std::shared_ptr<MemoryLocation<MInt>> makeStruct(
+            typename StructLocation<MInt, Element...>::LocPtr ptr,
+            typename StructLocation<MInt, Element...>::IntervalPtr offset) {
+        return std::make_shared<StructLocation<MInt, Element...>>(ptr, offset);
+    }
+
+    template <typename MInt, typename ...Element>
+    static std::shared_ptr<MemoryLocation<MInt>> makeStruct(
+            typename StructLocation<MInt, Element...>::LocPtr ptr,
+            const typename StructLocation<MInt, Element...>::Offsets& offsets) {
+        return std::make_shared<StructLocation<MInt, Element...>>(ptr, offsets);
+    }
 
     bool kind() const { return this->kind_; }
     virtual const std::unordered_set<IntervalPtr>& offsets() const = 0;
 
     bool isArray() const { return this->kind_ == ARRAY; }
     bool isStruct() const { return this->kind_ == STRUCT; }
+    bool isNull() const { return this->kind_ == NULL_; }
 
     static bool classof(const MemoryLocation*) {
         return true;
     }
+
+    template <typename Elem>
+    typename Elem::Ptr load(IntervalPtr interval) const {
+        if (this->isArray()) {
+            return llvm::dyn_cast<ArrayLocation<MachineInt, Elem>>(this)->load(interval);
+        } else if (this->isStruct()) {
+            return nullptr;
+        } else if (this->isNull()) {
+            return llvm::dyn_cast<NullLocation<MachineInt>>(this)->load(interval);
+        } else {
+            UNREACHABLE("Unknown location type");
+        }
+    }
+
+    template <typename Elem>
+    void store(IntervalPtr, typename Elem::Ptr) {}
+
+};
+
+template <typename MachineInt>
+class NullLocation : public MemoryLocation<MachineInt> {
+public:
+
+    using Base = MemoryLocation<MachineInt>;
+    using BasePtr = typename Base::Ptr;
+    using BaseConstPtr = typename Base::ConstPtr;
+
+    using IntervalT = Interval<MachineInt>;
+    using IntervalPtr = typename IntervalT::Ptr;
+
+private:
+
+    NullLocation() = default;
+
+public:
+
+    NullLocation(const NullLocation&) = delete;
+    NullLocation(NullLocation&&) = delete;
+    NullLocation& operator=(const NullLocation&) = delete;
+    NullLocation& operator=(NullLocation&&) = delete;
+
+    static BasePtr instance() {
+        static BasePtr instance_ = std::make_shared<NullLocation>();
+        return instance_;
+    }
+
+    static bool classof(const Base* other) {
+        return other->kind() == Base::NULL_;
+    }
+
+    const std::unordered_set<IntervalPtr>& offsets() const override {
+        return {};
+    }
+
+    bool isTop() const override { return false; }
+    bool isBottom() const override { return false; }
+
+    void setTop() override {}
+    void setBottom() override {}
+
+    bool leq(BaseConstPtr) const override {
+        UNREACHABLE("Unimplemented");
+    }
+
+    bool equals(BaseConstPtr other) const override {
+        return other->isNull();
+    }
+
+    void joinWith(BaseConstPtr other) override {
+        ASSERTC(other->isNull());
+    }
+
+    void meetWith(BaseConstPtr other) override {
+        ASSERTC(other->isNull());
+    }
+
+    void widenWith(BaseConstPtr other) override {
+        joinWith(other);
+    }
+
+    size_t hashCode() const override { return 42; }
+    std::string toString() const override { return "null"; }
+
+    template <typename Elem>
+    typename Elem::Ptr load(IntervalPtr) const {
+        return Elem::top();
+    }
+
+    template <typename Elem>
+    void store(IntervalPtr, typename Elem::Ptr) {}
 };
 
 template <typename MachineInt, typename Element>
@@ -70,6 +198,7 @@ public:
     ArrayLocation(ArrayLocation&&) = default;
     ArrayLocation& operator=(const ArrayLocation&) = default;
     ArrayLocation& operator=(ArrayLocation&&) = default;
+    ~ArrayLocation() override = default;
 
     static bool classof(const Base* other) {
         return other->kind() == Base::ARRAY;
@@ -163,6 +292,7 @@ public:
     StructLocation(StructLocation&&) = default;
     StructLocation& operator=(const StructLocation&) = default;
     StructLocation& operator=(StructLocation&&) = default;
+    ~StructLocation() override = default;
 
     static bool classof(const Base* other) {
         return other->kind() == Base::STRUCT;
--- Interpreter/Domain/Memory/Pointer.hpp
+++ Interpreter/Domain/Memory/Pointer.hpp
@@ -14,23 +14,157 @@
 namespace borealis {
 namespace absint {
 
-//template <typename MachineInt>
-//class Pointer : public AbstractDomain<Pointer<MachineInt>> {
-//public:
-//
-//    using Ptr = std::shared_ptr<Pointer<MachineInt>>;
-//    using ConstPtr = std::shared_ptr<const Pointer<MachineInt>>;
-//
-//    using IntervalT = Interval<MachineInt>;
-//    using IntervalPtr = typename IntervalT::Ptr;
-//    using Location = MemoryLocation<MachineInt>;
-//    using PointsToSet = std::unordered_set<Location>;
-//
-//private:
-//public:
-//
-//
-//};
+template <typename MachineInt>
+class Pointer : public AbstractDomain<Pointer<MachineInt>> {
+public:
+
+    using Ptr = std::shared_ptr<Pointer<MachineInt>>;
+    using ConstPtr = std::shared_ptr<const Pointer<MachineInt>>;
+
+    using IntervalT = Interval<MachineInt>;
+    using IntervalPtr = typename IntervalT::Ptr;
+    using Location = MemoryLocation<MachineInt>;
+    using LocationPtr = typename Location::Ptr;
+    using PointsToSet = std::unordered_set<LocationPtr>;
+
+private:
+
+    bool isBottom_;
+    PointsToSet ptsTo_;
+
+private:
+    struct TopTag{};
+    struct BottomTag{};
+
+    explicit Pointer(TopTag) : isBottom_(false) {}
+    explicit Pointer(BottomTag) : isBottom_(true) {}
+
+public:
+
+    Pointer() : Pointer(TopTag{}) {}
+    Pointer(const Pointer&) = default;
+    Pointer(Pointer&&) = default;
+    Pointer& operator=(const Pointer&) = default;
+    Pointer& operator=(Pointer&&) = default;
+    ~Pointer() override = default;
+
+    bool isTop() const override { return (not this->isBottom_) and this->ptsTo_.empty(); }
+    bool isBottom() const override { return this->isBottom_ and this->ptsTo_.empty(); }
+
+    bool isNull() const { return this->ptsTo_.size() == 1 and this->ptsTo_.begin()->isNull(); }
+    bool ptsToNull() const {
+        for (auto&& it : this->ptsTo_) {
+            if (it->isNull()) return true;
+        }
+        return false;
+    }
+
+    void setTop() override {
+        this->isBottom_ = false;
+        this->ptsTo_.clear();
+    }
+
+    void setBottom() override {
+        this->isBottom_ = true;
+        this->ptsTo_.clear();
+    }
+
+    bool leq(ConstPtr) const override {
+        UNREACHABLE("Unimplemented");
+    }
+
+    bool equals(ConstPtr other) const override {
+        if (this->isTop()) {
+            return other->isTop();
+        } else if (this->isBottom()) {
+            return other->isBottom();
+        } else if (other->isTop() || other->isBottom()) {
+            return false;
+        } else if (this->isNull()) {
+            return other->isNull();
+        } else {
+
+            if (this->ptsTo_.size() != other->ptsTo_.size()) return false;
+
+            for (auto&& it : other->ptsTo_) {
+                auto&& its = this->ptsTo_.find(it);
+                if (its == this->ptsTo_.end()) return false;
+                if (not util::equal_with_find(it->offsets(), its->offsets(), LAM(a, a), LAM2(a, b, a->equals(b))))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    void joinWith(ConstPtr other) override {
+        if (this->isBottom()) {
+            this->operator=(*other.get());
+        } else if (this->isTop()) {
+            return;
+        } else if (other->isBottom()) {
+            return;
+        } else if (other->isTop()) {
+            this->setTop();
+        } else {
+
+            for (auto&& it : other->ptsTo_) {
+                auto&& its = this->ptsTo_.find(it);
+                if (its == this->ptsTo_.end()) {
+                    this->ptsTo_.insert(it);
+                } else {
+                    its->joinWith(it);
+                }
+            }
+        }
+    }
+
+    void widenWith(ConstPtr other) override { joinWith(other); }
+
+    void meetWith(ConstPtr other) override {
+        if (this->isBottom()) {
+            return;
+        } else if (this->isTop()) {
+            this->operator=(*other.get());
+        } else if (other->isBottom()) {
+            this->setBottom();
+        } else if (other->isTop()) {
+            return;
+        } else {
+
+            for (auto&& it : this->ptsTo_) {
+                if (not util::contains(other->ptsTo_, it)) {
+                    this->ptsTo_.erase(it);
+                }
+            }
+
+            for (auto&& it : other->ptsTo_) {
+                auto&& its = this->ptsTo_.find(it);
+                if (its != this->ptsTo_.end()) {
+                    its->meetWith(it);
+                }
+            }
+        }
+    }
+
+    size_t hashCode() const override {
+        return util::hash::defaultHasher()(this->isBottom_, this->ptsTo_);
+    }
+
+    std::string toString() const {
+        std::stringstream ss;
+        ss << "Ptr [";
+        if (this->isTop()) ss << " TOP ]";
+        else if (this->isBottom()) ss << " BOTTOM ]";
+        else {
+            for (auto&& it : this->ptsTo_) {
+                ss << std::endl << it->toString();
+            }
+            ss << std::endl << "]";
+        }
+        return ss.str();
+    }
+
+};
 
 } // namespace absint
 } // namespace borealis
